416ffa3b74ff1a9f0322e4398e7c7692ebdfe7aa
Sean <seanbatten58@gmail.com>
2023-03-24 19:21:55 +0000

    Passing cars and missing integers

---
+++ csharp/MissingInteger/Program.cs
@@ -0,0 +1,46 @@
+﻿// https://app.codility.com/programmers/lessons/4-counting_elements/missing_integer/
+Console.WriteLine("Hello, World!");
+
+var missing = new Solution();
+var a = missing.solution(new[]{1,3,6,4,1,2});
+var b = missing.solution(new[]{-1,-3});
+Console.WriteLine("done");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        var min = 0;
+        var max = 0;
+
+        var data = new System.Collections.Generic.HashSet<int>();
+
+        foreach(var value in A)
+        {
+            if(value < 1) continue;
+
+            if(data.Add(value))
+            {
+                if(min == 0 || value < min) min = value;
+                max = Math.Max(max, value);
+            }
+        }
+
+        // If we've not found any positive numbers, or we don't have a 1 then it's easy
+        if(data.Count == 0) return 1;
+        if(min > 1) return 1;
+
+        // If we've got a all the numbers then we need 1 more than the max
+        var range = max - min;
+        if(data.Count == range + 1) return max + 1;
+
+        // There's something missing between the min and max
+        for(var i = min + 1; i < max; i++)
+        {
+            if(data.Contains(i) == false) return i;
+        }
+
+        // Wont' get here
+        return 0;
+    }
+}
---
+++ csharp/PassingCars/Program.cs
@@ -0,0 +1,35 @@
+﻿// https://app.codility.com/programmers/lessons/5-prefix_sums/passing_cars/
+Console.WriteLine("Hello, World!");
+
+var missing = new Solution();
+var a = missing.solution(new[]{0,1,0,1,1});
+var b = missing.solution(new[]{0,0});
+var c = missing.solution(new[]{1,0});
+Console.WriteLine("done");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        var count = 0;
+        var adjustment = 0;
+
+        foreach(var car in A)
+        {
+            if(car == 0)
+            {
+                // Something going east
+                adjustment++;
+            }
+            else if(car == 1)
+            {
+                // Something going west
+                count += adjustment;
+            }
+
+            if(count > 1_000_000_000) return -1;
+        }
+
+        return count;
+    }
+}
