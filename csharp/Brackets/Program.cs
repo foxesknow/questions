4f02bc03ae9ac23fe468b84eaf36572059dfab23
Sean <seanbatten58@gmail.com>
2023-03-26 16:10:53 +0100

    Brackets

---
+++ csharp/Brackets/Program.cs
@@ -0,0 +1,41 @@
+﻿// https://app.codility.com/programmers/lessons/7-stacks_and_queues/brackets/
+
+
+var brackets = new Solution();
+var a = brackets.solution("{[()()]}");
+var b = brackets.solution("([)()]");
+
+Console.WriteLine("done");
+
+class Solution
+{
+    public int solution(string S)
+    {
+        if(S.Length == 0) return 1;
+
+        var stack = new System.Collections.Generic.Stack<char>();
+
+        foreach(var c in S)
+        {
+            if(c == '{' || c == '[' || c == '(')
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if(stack.Count == 0)
+                {
+                    // Oops!
+                    return 0;
+                }
+
+                var top = stack.Pop();
+                if(top == '{' && c != '}') return 0;
+                if(top == '[' && c != ']') return 0;
+                if(top == '(' && c != ')') return 0;
+            }
+        }
+
+        return stack.Count == 0 ? 1 : 0;
+    }
+}
