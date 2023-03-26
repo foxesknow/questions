4e66abee8f19f6d2e4a98e88ac52630ec2666e2e
Sean <seanbatten58@gmail.com>
2023-03-26 14:24:31 +0100

    More questions

---
+++ csharp/Distinct/Program.cs
@@ -0,0 +1,30 @@
+﻿// https://app.codility.com/programmers/lessons/6-sorting/distinct/
+
+var distinct = new Solution();
+var a = distinct.solution(new[]{1,1,1,2,3,1});
+Console.WriteLine("done");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        if(A.Length == 0) return 0;
+        if(A.Length == 1) return 1;
+
+        Array.Sort(A);
+
+        var distinctCount = 1;
+        var lastNumber = A[0];
+
+        for(int i = 1; i < A.Length; i++)
+        {
+            if(lastNumber != A[i])
+            {
+                lastNumber = A[i];
+                distinctCount++;
+            }
+        }
+
+        return distinctCount;
+    }
+}
---
+++ csharp/MinAvgTwoSlice/Program.cs
@@ -0,0 +1,16 @@
+﻿// https://app.codility.com/programmers/lessons/5-prefix_sums/min_avg_two_slice/
+
+var twoSlice = new Solution();
+var a = twoSlice.solution(new[]{4,2,2,5,1,5,8});
+
+Console.WriteLine("done");
+
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        //TODO
+        return 0;
+    }
+}
