c7552becd5828df841990ece6aecc400a852f20d
Sean <seanbatten58@gmail.com>
2023-03-26 17:06:25 +0100

    Nesting

---
+++ csharp/Nesting/Program.cs
@@ -0,0 +1,37 @@
+﻿// https://app.codility.com/programmers/lessons/7-stacks_and_queues/nesting/
+
+var nesting = new Solution();
+var a = nesting.solution("(()(())())");
+var b = nesting.solution("())");
+var c = nesting.solution("()()()");
+
+Console.WriteLine("done");
+
+class Solution
+{
+    public int solution(string S)
+    {
+        if(S.Length == 0) return 1;
+
+        var openCount = 0;
+
+        foreach(var c in S)
+        {
+            if(c == '(')
+            {
+                openCount++;
+            }
+            else
+            {
+                if(openCount == 0)
+                {
+                    return 0;
+                }
+
+                openCount--;
+            }
+        }
+
+        return openCount == 0 ? 1 : 0;
+    }
+}
