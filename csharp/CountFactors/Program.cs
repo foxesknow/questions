e3d12c022646c4b2732b68713d4bc6e37691355b
Sean <seanbatten58@gmail.com>
2023-03-30 16:33:06 +0100

    Factors and perimeters

---
+++ csharp/CountFactors/Program.cs
@@ -0,0 +1,43 @@
+﻿// https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/count_factors/
+
+var factors = new Solution();
+var a = factors.solution(1);
+var b = factors.solution(2);
+var c = factors.solution(6);
+var d = factors.solution(12);
+var e = factors.solution(3);
+var f = factors.solution(7);
+var g = factors.solution(120);
+var h = factors.solution(16);
+
+Console.WriteLine("done");
+
+class Solution
+{
+    public int solution(int N)
+    {
+        if(N == 1) return 1;
+
+        var count = 0;
+
+        var root = (int)Math.Sqrt(N);
+        for(int i = 1; i <= root; i++)
+        {
+            if((N % i) == 0) 
+            {
+                // Most factors are X * Y, giving us 2 factors each pass.
+                // However, if the original number is a square then it'll be X * X, giving us just the one factor
+                if(i * i == N)
+                {
+                    count += 1;
+                }
+                else
+                {
+                    count += 2;
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ csharp/MinPerimeterRectangle/Program.cs
@@ -0,0 +1,28 @@
+﻿// https://app.codility.com/programmers/lessons/10-prime_and_composite_numbers/min_perimeter_rectangle/
+
+var perimeter = new Solution();
+var a = perimeter.solution(30);
+var b = perimeter.solution(36);
+var c = perimeter.solution(1);
+
+Console.WriteLine("done");
+
+class Solution
+{
+    public int solution(int N)
+    {
+        var root = (int)Math.Sqrt(N);
+
+        for(int length = root; length > 0; length--)
+        {
+            var remainder = N % length;
+            if(remainder != 0) continue;
+
+            var height = N / length;
+
+            return (height * 2) + (length * 2);
+        }
+
+        return 0;
+    }
+}
