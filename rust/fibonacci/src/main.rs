866c618b0e63202b4c753eae245dee6fdeb7e24b
sean <seanbatten58@gmail.com>
2023-04-08 17:46:50 +0100

    Additional fibanacci code

--- rust/fibonacci/src/main.rs
+++ rust/fibonacci/src/main.rs
@@ -1,4 +1,7 @@
-fn fib(n : u128) -> u128
+use std::{collections::HashMap};
+
+// The canonical fibanacci implementation
+fn fib(n : u32) -> u128
 {
     match n
     {
@@ -8,8 +11,87 @@ fn fib(n : u128) -> u128
     }
 }
 
+// Working out the fibanacci number by starting from 0 and moving forward
+fn fib_seq(n : u32) -> u128
+{
+    let mut current : u128 = 0;
+    let mut next : u128 = 1;
+    let mut count: u32 = n;
+
+    while count != 0
+    {
+        let (new_current, new_next) = (next, next + current);
+        current = new_current;
+        next = new_next;
+        count -= 1;
+    }
+
+    current
+}
+
+// Using memoization to work out the value
+fn fib_mem(n : u32) -> u128
+{
+    let mut cache: HashMap<u32, u128> = HashMap::new();
+    cache.insert(0, 0);
+    cache.insert(1, 1);
+
+    return execute(&mut cache, n);
+
+    fn execute(cache: &mut HashMap<u32, u128>, n: u32) -> u128
+    {
+        if let Some(&number) = cache.get(&n) 
+        {
+            return number;
+        }
+        else
+        {
+            let x = execute(cache, n - 1) + execute(cache, n - 2);
+            cache.insert(n, x);
+            return x;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests
+{
+    use super::*;
+
+    #[test]
+    fn test_fib()
+    {
+        assert_eq!(fib(0), 0);
+        assert_eq!(fib(1), 1);
+        assert_eq!(fib(1), 1);
+        assert_eq!(fib(5), 5);
+        assert_eq!(fib(10), 55);
+    }
+
+    #[test]
+    fn test_fib_seq()
+    {
+        assert_eq!(fib_seq(0), 0);
+        assert_eq!(fib_seq(1), 1);
+        assert_eq!(fib_seq(1), 1);
+        assert_eq!(fib_seq(5), 5);
+        assert_eq!(fib_seq(10), 55);
+    }
+
+    #[test]
+    fn test_fib_mem()
+    {
+        assert_eq!(fib_mem(0), 0);
+        assert_eq!(fib_mem(1), 1);
+        assert_eq!(fib_mem(1), 1);
+        assert_eq!(fib_mem(5), 5);
+        assert_eq!(fib_mem(10), 55);
+    }
+}
 
 fn main() 
 {
-    println!("value is {}", fib(8));
+    println!("fib is {}", fib(40));
+    println!("fib_mem is {}", fib_mem(40));
+    println!("fib_seq is {}", fib_seq(40));
 }
