ac5fda693617607f8e1fab8b820248ef23dc7a5c
sean <seanbatten58@gmail.com>
2023-06-21 12:32:16 +0100

    Path questions

---
+++ python/112-PathSum/main.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+import unittest
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return False
+
+        # We're at a leaf node
+        if root.left is None and root.right is None and targetSum - root.val == 0:
+            return True
+
+        if root.left and self.hasPathSum(root.left, targetSum - root.val) :
+            return True
+
+        if root.right and self.hasPathSum(root.right, targetSum - root.val) :
+            return True
+
+        return False
+
+    
+class Tests(unittest.TestCase):
+    def test_example1(self):
+        solution = Solution()
+        
+        root = TreeNode(1, TreeNode(2), TreeNode(3))
+        self.assertTrue(solution.hasPathSum(root, 3))
+
+    def test_example2(self):
+        solution = Solution()
+        
+        root = TreeNode(1, TreeNode(2), TreeNode(3))
+        self.assertTrue(solution.hasPathSum(root, 4))
+
+    def test_example3(self):
+        solution = Solution()
+        
+        root = None
+        self.assertFalse(solution.hasPathSum(root, 0))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ python/113-PathSum2/main.py
@@ -0,0 +1,57 @@
+from typing import Optional
+from typing import List
+
+import unittest
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        result: List[List[int]] = []
+        self.walk(root, targetSum, [], result)
+
+        return result
+
+    def walk(self, root: Optional[TreeNode], targetSum: int, path: List[int], acc: List[List[int]]):
+        if root is None:
+            return
+
+        path.append(root.val)
+
+        try:
+            # We're at a leaf node
+            if root.left is None and root.right is None and targetSum - root.val == 0:
+                clone = path.copy()
+                acc.append(clone)
+
+            if root.left:
+                self.walk(root.left, targetSum - root.val, path, acc)
+
+            if root.right:
+                self.walk(root.right, targetSum - root.val, path, acc)
+        finally:
+            path.pop(len(path) - 1)
+
+    
+class Tests(unittest.TestCase):
+    def test_example1(self):
+        solution = Solution()
+        
+        root = TreeNode(1, TreeNode(2), TreeNode(3))
+        paths = solution.pathSum(root, 3)
+        print(paths)
+
+    def test_example1(self):
+        solution = Solution()
+        
+        root = TreeNode(1, TreeNode(2), TreeNode(1, TreeNode(1)))
+        paths = solution.pathSum(root, 3)
+        print(paths)
+
+
+if __name__ == '__main__':
+    unittest.main()
