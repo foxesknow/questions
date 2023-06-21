9d90606422e3e4f20eb97332dc212ac61338cb23
sean <seanbatten58@gmail.com>
2023-06-21 12:57:38 +0100

    Question 114

---
+++ python/114-FlattenBinaryTree/main.py
@@ -0,0 +1,69 @@
+from typing import Optional
+from typing import List
+
+import unittest
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]):
+        """
+        Do not return anything, modify root in-place instead.
+        """
+
+        if not root:
+            return None
+
+        head = None
+        next = None
+
+        for node in self.pre_order(root):
+            if head is None:
+                head = node
+                next = node
+            else:
+                next.right = node
+                next = next.right
+
+
+    def pre_order(self, root: Optional[TreeNode]):
+        if root:
+            left = root.left
+            right = root.right
+
+            root.left = None
+            root.right = None
+
+            yield root
+            if left: yield from self.pre_order(left)
+            if right: yield from self.pre_order(right)
+
+class Tests(unittest.TestCase):
+    def test_example1(self):
+        solution = Solution()
+        
+        root = TreeNode(1, TreeNode(2), TreeNode(3))
+        solution.flatten(root)
+        self.assertEqual(root.val, 1)
+        self.assertEqual(root.right.val, 2)
+        self.assertEqual(root.right.right.val, 3)
+
+    def test_example2(self):
+        solution = Solution()
+        
+        root = TreeNode(1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(5, None, TreeNode(5)))
+        solution.flatten(root)
+        self.assertEqual(root.val, 1)
+        self.assertEqual(root.right.val, 2)
+        self.assertEqual(root.right.right.val, 3)
+        self.assertEqual(root.right.right.right.val, 4)
+        self.assertEqual(root.right.right.right.right.val, 5)
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
