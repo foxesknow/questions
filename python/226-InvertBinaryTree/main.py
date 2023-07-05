c999bc61f6228a1155d83b3131402030be3d66a6
sean <seanbatten58@gmail.com>
2023-07-05 12:31:38 +0100

    Question 226

---
+++ python/226-InvertBinaryTree/main.py
@@ -0,0 +1,47 @@
+from typing import Optional, Self
+
+import unittest
+
+class TreeNode:
+    def __init__(self, val: int = 0, left: Optional[Self] =None, right: Optional[Self] = None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            tmp = root.left
+            root.left = root.right
+            root.right = tmp
+
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+
+        return root
+    
+    def invertTree_stack(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            stack = [root]
+            while len(stack):
+                next = stack.pop()
+
+                tmp = next.left
+                next.left = next.right
+                next.right = tmp
+
+                if next.left: stack.append(next.left)
+                if next.right: stack.append(next.right)
+
+        return root
+    
+class Tests(unittest.TestCase):
+    def test_example1(self):
+        solution = Solution()
+
+        tree = TreeNode(2, TreeNode(1), TreeNode(3))
+        new_tree = solution.invertTree(tree)
+        self.assertIsNotNone(new_tree)
+
+
+if __name__ == '__main__':
+    unittest.main()
