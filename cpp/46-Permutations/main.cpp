239d294e3c0c8129b3a6d4fe0eb130fc27339978
sean <seanbatten58@gmail.com>
2023-07-25 16:26:08 +0100

    Question 46

---
+++ cpp/46-Permutations/main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution 
+{
+public:
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+
+        do
+        {
+            result.push_back(nums);
+        }while(next_permutation(nums.begin(), nums.end()));
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v = {0,-1,1};
+    auto result = s.permute(v);
+}
