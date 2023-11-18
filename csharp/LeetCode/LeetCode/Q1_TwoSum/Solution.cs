f4d488342366be62ddc70d13bb52572f2b99299e
Sean Batten <seanbatten58@gmail.com>
2023-11-18 10:57:50 +0000

    Question 1

---
+++ csharp/LeetCode/LeetCode/Q1_TwoSum/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Q1_TwoSum
+{
+    public class Solution 
+    {
+        public int[] TwoSum(int[] nums, int target) 
+        {
+            var indexes = new Dictionary<int, int>();
+
+            for(var i = 0; i < nums.Length; i++)
+            {
+                var value = nums[i];
+                if(indexes.TryGetValue(target - value, out var existing))
+                {
+                    return new int[]{existing, i};
+                }
+                else
+                {
+                    indexes.Add(value, i);
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q1_TwoSum/SolutionTests.cs
@@ -0,0 +1,53 @@
+﻿using LeetCode.Q1_TwoSum;
+
+using NUnit.Framework;
+
+
+namespace LeetCodeTests.Q1_TwoSum
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var solution = new Solution();
+            var result = solution.TwoSum([2, 7, 11, 15], 9);
+            
+            Assert.That(result.Length, Is.EqualTo(2));
+            Assert.That(result, Contains.Item(0));
+            Assert.That(result, Contains.Item(1));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var solution = new Solution();
+            var result = solution.TwoSum([3, 2, 4], 6);
+            
+            Assert.That(result.Length, Is.EqualTo(2));
+            Assert.That(result, Contains.Item(1));
+            Assert.That(result, Contains.Item(2));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var solution = new Solution();
+            var result = solution.TwoSum([3, 3], 6);
+            
+            Assert.That(result.Length, Is.EqualTo(2));
+            Assert.That(result, Contains.Item(0));
+            Assert.That(result, Contains.Item(1));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var solution = new Solution();
+            var result = solution.TwoSum([3, 4], 8);
+            
+            Assert.That(result.Length, Is.EqualTo(0));
+        }
+    }
+}
