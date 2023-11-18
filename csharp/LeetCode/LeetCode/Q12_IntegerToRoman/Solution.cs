2c5368f3e67e86c82a8027ffa119e6e7344dbe7d
Sean Batten <seanbatten58@gmail.com>
2023-11-18 11:43:59 +0000

    Question 12

---
+++ csharp/LeetCode/LeetCode/Q12_IntegerToRoman/Solution.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q12_IntegerToRoman
+{
+    public class Solution
+    {
+        public string IntToRoman(int num) 
+        {
+            var roman = new StringBuilder(10);
+
+            while(num >= 1000)
+            {
+                roman.Append('M');
+                num -= 1000;
+            }
+
+            if(num >= 900)
+            {
+                roman.Append("CM");
+                num -= 900;
+            }
+
+            if(num >= 500)
+            {
+                roman.Append('D');
+                num -= 500;
+            }
+
+            if(num >= 400)
+            {
+                roman.Append("CD");
+                num -= 400;
+            }
+
+            while(num >= 100)
+            {
+                roman.Append('C');
+                num -= 100;
+            }
+
+            if(num >= 90)
+            {
+                roman.Append("XC");
+                num -= 90;
+            }
+
+            while(num >= 50)
+            {
+                roman.Append('L');
+                num -= 50;
+            }
+
+            if(num >= 40)
+            {
+                roman.Append("XL");
+                num -= 40;
+            }
+
+            while(num >= 10)
+            {
+                roman.Append('X');
+                num -= 10;
+            }
+
+            if(num >= 9)
+            {
+                roman.Append("IX");
+                num  -= 9;
+            }
+
+            if(num >= 5)
+            {
+                roman.Append('V');
+                num -= 5;
+            }
+
+            if(num >= 4)
+            {
+                roman.Append("IV");
+                num -= 4;
+            }
+
+            for(var i = 0; i < num; i++)
+            {
+                roman.Append('I');
+            }
+
+            return roman.ToString();
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q12_IntegerToRoman/SolutionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q12_IntegerToRoman;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q12_IntegerToRoman
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var solution = new Solution();
+            Assert.That(solution.IntToRoman(3), Is.EqualTo("III"));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var solution = new Solution();
+            Assert.That(solution.IntToRoman(58), Is.EqualTo("LVIII"));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var solution = new Solution();
+            Assert.That(solution.IntToRoman(1994), Is.EqualTo("MCMXCIV"));
+        }
+    }
+}
