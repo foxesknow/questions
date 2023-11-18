ce8378dfe27bd3955b7e04dc931e3c7621b804d9
Sean Batten <seanbatten58@gmail.com>
2023-11-18 12:02:49 +0000

    Question 100

---
+++ csharp/LeetCode/LeetCode/Q100_SameTree/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q100_SameTree
+{
+    public class Solution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q) 
+        {
+            var stack = new Stack<(TreeNode, TreeNode)>();
+            stack.Push((p, q));
+
+            while(stack.TryPop(out var nodes))
+            {
+                var (left, right) = nodes;
+
+                if(left is null && right is null) continue;
+                
+                if(left is null || right is null) return false;
+                if(left.val != right.val) return false;
+
+                stack.Push((left.left, right.left));
+                stack.Push((left.right, right.right));
+            }
+
+            return true;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q100_SameTree/SolutionTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode;
+using LeetCode.Q100_SameTree;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q100_SameTree
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var left = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            var right = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            
+            var solution = new Solution();
+            Assert.That(solution.IsSameTree(left, right), Is.True);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var left = new TreeNode(1, new TreeNode(2), null);
+            var right = new TreeNode(1, null, new TreeNode(2));
+            
+            var solution = new Solution();
+            Assert.That(solution.IsSameTree(left, right), Is.False);
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var left = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+            var right = new TreeNode(1, new TreeNode(1), new TreeNode(2));
+            
+            var solution = new Solution();
+            Assert.That(solution.IsSameTree(left, right), Is.False);
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var left = new TreeNode(1, new TreeNode(2), null);
+            var right = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+            
+            var solution = new Solution();
+            Assert.That(solution.IsSameTree(left, right), Is.False);
+        }
+    }
+}
