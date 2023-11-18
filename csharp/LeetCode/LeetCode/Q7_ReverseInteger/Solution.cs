4abfc512b4eb998d6df41a9d477d007509dd4519
Sean Batten <seanbatten58@gmail.com>
2023-11-18 11:33:48 +0000

    Question 7

---
+++ csharp/LeetCode/LeetCode/Q7_ReverseInteger/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q7_ReverseInteger
+{
+    public class Solution
+    {
+        public int Reverse(int x) 
+        {
+            var isNegative = (x < 0);
+            var value = 0;
+
+            while(x != 0)
+            {
+                var nextDigit = Math.Abs(x % 10);
+
+                // Check for overflow
+                if(value != 0 && ((int.MaxValue - nextDigit) / value) < 10) return 0;
+
+                value = (value * 10) + nextDigit;
+                x /= 10;
+            }
+
+            // A positive number always has a negative equivilant
+            if(isNegative) value *= -1;
+
+            return value;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q7_ReverseInteger/SolutionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q7_ReverseInteger;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q7_ReverseInteger
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var solution = new Solution();
+            Assert.That(solution.Reverse(123), Is.EqualTo(321));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var solution = new Solution();
+            Assert.That(solution.Reverse(-123), Is.EqualTo(-321));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var solution = new Solution();
+            Assert.That(solution.Reverse(120), Is.EqualTo(21));
+        }
+    }
+}
