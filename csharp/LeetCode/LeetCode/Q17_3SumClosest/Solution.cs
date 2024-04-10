c3e1e6dc9cf98380eda72e13cc16be1e2736e6f6
Sean <seanbatten58@gmail.com>
2024-04-10 17:42:06 +0100

    Question 17

---
+++ csharp/LeetCode/LeetCode/Q17_3SumClosest/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q17_3SumClosest
+{
+    public class Solution
+    {
+        public int ThreeSumClosest(int[] nums, int target) 
+        {
+            int? closestDiff = null;
+            int closest = 0;
+
+            // Sort the numbers so it's easier to find the matches
+            Array.Sort(nums);
+
+            int lastIndex = nums.Length - 1;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                var first = nums[i];
+
+                // It's the same start number as last time, so don't bother again
+                if(i > 0 && first == nums[i - 1]) continue;
+
+                var left = i + 1;
+                var right = lastIndex;
+
+                while(left < right)
+                {
+                    var second = nums[left];
+                    var third = nums[right];
+
+                    // If the first 2 are bigger than zero then we can stop checking
+                    var sum = first + second + third;                    
+                    if(sum == target) return sum;
+
+                    var diff = Math.Abs(sum - target);
+
+                    if(closestDiff == null)
+                    {
+                        closestDiff = diff;
+                        closest = sum;
+                    }
+                    else
+                    {
+                        if(closestDiff.Value > diff)
+                        {
+                            closestDiff = diff;
+                            closest = sum;
+                        }
+                    }
+
+                    if(sum > target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        left++;
+                    }
+                }
+            }
+
+            return closest;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q17_3SumClosest/SolutionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q17_3SumClosest;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q17_3SumClosest
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var sum = s.ThreeSumClosest(new[]{-1,2,1,-4}, 1);
+            Assert.That(sum, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var sum = s.ThreeSumClosest(new[]{0,0,0}, 1);
+            Assert.That(sum, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var sum = s.ThreeSumClosest(new[]{1,1,1}, 0);
+            Assert.That(sum, Is.EqualTo(3));
+        }
+    }
+}
