37155f16455149931c76203c33a8dfcda1338df0
Sean <seanbatten58@gmail.com>
2024-04-12 11:08:59 +0100

    Question 19

---
+++ csharp/LeetCode/LeetCode/Q19_RemoveNthNode/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q19_RemoveNthNode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n) 
+        {            
+            var secondHead = head;
+            for(var i = 0; i < n; i++)
+            {
+                secondHead = secondHead.next;
+            }
+
+            var front = new ListNode(0, head);
+            head = front;
+            while(secondHead != null)
+            {
+                head = head.next;
+                secondHead = secondHead.next;
+            }
+
+            head.next = head?.next?.next;
+
+            return front.next;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q19_RemoveNthNode/SolutionTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+using LeetCode.Q19_RemoveNthNode;
+
+namespace LeetCodeTests.Q19_RemoveNthNode
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            ListNode n = new(1, new(2, new(3, new(4, new(5)))));
+            var s = new Solution();
+            var node = s.RemoveNthFromEnd(n, 2);
+            Assert.That(node, Is.Not.Null);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            ListNode n = new(1);
+            var s = new Solution();
+            var node = s.RemoveNthFromEnd(n, 1);
+            Assert.That(node, Is.Null);
+        }
+
+        [Test]
+        public void Example3()
+        {
+            ListNode n = new(1, new(2));
+            var s = new Solution();
+            var node = s.RemoveNthFromEnd(n, 1);
+            Assert.That(node, Is.Not.Null);
+        }
+    }
+}
