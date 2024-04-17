55f8043a8e74a987e1149b0f2ec58962ac47713b
foxesknow <seanbatten58@gmail.com>
2024-04-17 12:41:52 +0100

    Account merging

---
+++ csharp/LeetCode/LeetCode/AccountsMerge.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class AccountsMerge
+    {
+        public List<List<string>> Merge(Dictionary<string, List<string>> contactsToEmail)
+        {
+            Dictionary<string, List<string>> emailToContacts = new();
+
+            foreach(var (contact, addresses) in contactsToEmail)
+            {
+                foreach(var address in addresses)
+                {
+                    if(emailToContacts.TryGetValue(address, out var contacts))
+                    {
+                        contacts.Add(contact);
+                    }
+                    else
+                    {
+                        emailToContacts[address] = new(){contact};
+                    }
+                }
+            }
+
+            var merge = new List<List<string>>();
+            HashSet<string> seen = new();
+
+            foreach(var (contact, emails) in contactsToEmail)
+            {
+                if(seen.Contains(contact)) continue;
+
+                List<string> group = new();                
+
+                foreach(var email in emails)
+                {
+                    WalkAddress(email, seen, group, contactsToEmail, emailToContacts);
+                }
+
+                merge.Add(group);
+            }
+
+            return merge;
+        }
+
+        private void WalkAddress(string address, HashSet<string> seen, List<string> group, Dictionary<string, List<string>> contactsToEmail, Dictionary<string, List<string>> emailToContacts)
+        {
+            var contacts = emailToContacts[address];
+
+            foreach(var contact in contacts)
+            {
+                if(seen.Contains(contact)) continue;
+                
+                seen.Add(contact);
+                group.Add(contact);
+
+                foreach(var email in contactsToEmail[contact])
+                {
+                    WalkAddress(email, seen, group, contactsToEmail, emailToContacts);
+                }
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/AccountsMergeTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class AccountsMergeTests
+    {
+        [Test]
+        public void Example1()
+        {
+            Dictionary<string, List<string>> contacts = new()
+            {
+                {"C1", new(){"bob@yahoo.com", "bob@gmail.com"}},
+                {"C2", new(){"mary@facebook.com"}},
+                {"C3", new(){"bob@gmail.com", "bob_1@hotmail.com"}},
+                {"C4", new(){"bob_1@hotmail.com"}},
+                {"C5", new(){"mary@facebook.com"}},
+                {"C6", new(){"mark@gmail.com"}},
+            };
+
+            var merge = new AccountsMerge();
+            var groups = merge.Merge(contacts);
+            Assert.That(groups, Has.Count.EqualTo(3));
+
+            Assert.That(groups[0], Contains.Item("C1") & Contains.Item("C3") & Contains.Item("C4"));
+            Assert.That(groups[1], Contains.Item("C2") & Contains.Item("C5"));
+            Assert.That(groups[2], Contains.Item("C6"));
+        }
+    }
+}
