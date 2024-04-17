d814c842b3f3d27e4baf7ecad2d3ed6d002b24a2
foxesknow <seanbatten58@gmail.com>
2024-04-17 16:02:19 +0100

    Question 128

---
+++ csharp/LeetCode/LeetCode/Q128_LongestConsequtiveSequence/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q128_LongestConsequtiveSequence
+{
+    public class Solution
+    {
+        public int LongestConsecutive(int[] nums) 
+        {
+            if(nums.Length == 0) return 0;
+
+            Array.Sort(nums);
+            var longest = 0;
+
+            int left = AdvanceLeft(0, nums);
+            int right = left;
+            int adjustment = 0;
+            
+            while(right + 1 < nums.Length)
+            {
+                if(nums[right] == nums[right + 1])
+                {
+                    // If adjacent items are the same then we ignore them.
+                    // This means we'll have to adjust the max by the number of items we skip
+                    adjustment++;
+                    right++;
+                }
+                else if(nums[right] + 1 == nums[right + 1])
+                {
+                    right++;
+                }
+                else
+                {
+                    longest = Math.Max(longest, (right - left) + 1 - adjustment);
+                    left = AdvanceLeft(right + 1, nums);
+                    right = left;
+                    adjustment = 0;
+                }
+            }
+
+            return Math.Max(longest, (right - left) + 1 - adjustment);
+        }
+
+        private int AdvanceLeft(int left, int[] nums)
+        {
+            while(left + 1 < nums.Length && nums[left] == nums[left + 1])
+            {
+                left++;
+            }
+
+            return left;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q128_LongestConsequtiveSequence/SolutionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q128_LongestConsequtiveSequence;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q128_LongestConsequtiveSequence
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var longest = s.LongestConsecutive(new[]{100,4,200,1,3,2});
+            Assert.That(longest, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var longest = s.LongestConsecutive(new[]{0,3,7,2,5,8,4,6,0,1});
+            Assert.That(longest, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var longest = s.LongestConsecutive(new[]{1,2,0,1});
+            Assert.That(longest, Is.EqualTo(3));
+        }
+    }
+}
