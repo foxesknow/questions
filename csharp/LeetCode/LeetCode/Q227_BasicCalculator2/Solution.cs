f07f0eb1de063fb89aac8472f8731f9be1fa3541
foxesknow <seanbatten58@gmail.com>
2024-04-17 13:26:24 +0100

    Question 227

---
+++ csharp/LeetCode/LeetCode/Q227_BasicCalculator2/Solution.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q227_BasicCalculator2
+{
+    public class Solution
+    {
+        public int Calculate(string s) 
+        {
+            var numbers = new Stack<int>();
+            var operators = new Stack<char>();
+
+            foreach(var token in Tokenize(s))
+            {
+                if(token is int i)
+                {
+                    numbers.Push(i);
+                }
+                else
+                {
+                    char op = (char)token;
+
+                    if(operators.Count == 0)
+                    {
+                        operators.Push(op);
+                    }
+                    else
+                    {
+                        if(Precedence(op) > Precedence(operators.Peek()))
+                        {
+                            operators.Push(op);
+                        }
+                        else
+                        {
+                            while(operators.Count > 0 && Precedence(op) <= Precedence(operators.Peek()))
+                            {
+                                Evaluate(operators.Pop(), numbers);
+                            }
+
+                            operators.Push(op);
+                        }
+                    }
+                }
+            }
+
+            while(operators.TryPop(out var op))
+            {
+                Evaluate(op, numbers);
+            }
+
+            return numbers.Peek();
+        }
+
+
+
+        private void Evaluate(char op, Stack<int> numbers)
+        {
+            var rhs = numbers.Pop();
+            var lhs = numbers.Pop();
+
+            var result = op switch
+            {
+                '+' => lhs + rhs,
+                '-' => lhs - rhs,
+                '*' => lhs * rhs,
+                '/' => lhs / rhs,
+                _   => throw new Exception("unsupported operator")
+            };
+
+            numbers.Push(result);
+        }
+
+        private int Precedence(char c)
+        {
+            if(c == '*' || c == '/') return 2;
+            return 1;
+        }
+
+        private IEnumerable<object> Tokenize(string s)
+        {
+            string current = "";
+
+            foreach(var c in s)
+            {
+                if(char.IsWhiteSpace(c)) continue;
+
+                if(char.IsDigit(c))
+                {
+                    current += c;
+                }
+                else
+                {
+                    // It's an operator
+                    if(current.Length != 0) yield return int.Parse(current);
+                    yield return c;
+
+                    current = "";
+                }
+            }
+
+            if(current.Length != 0) yield return int.Parse(current);
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q227_BasicCalculator2/SolutionTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q227_BasicCalculator2;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q227_BasicCalculator2
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("3+2*2", 7)]
+        [TestCase(" 3   +  2   *   2   ", 7)]
+        public void Example1(string expression, int expected)
+        {
+            var s = new Solution();
+            var result = s.Calculate(expression);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("3/2", 1)]
+        public void Example2(string expression, int expected)
+        {
+            var s = new Solution();
+            var result = s.Calculate(expression);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("2+3-4", 1)]
+        public void Example3(string expression, int expected)
+        {
+            var s = new Solution();
+            var result = s.Calculate(expression);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        [TestCase("1*2-3/4+5*6-7*8+9/10", -24)]
+        public void Example4(string expression, int expected)
+        {
+            var s = new Solution();
+            var result = s.Calculate(expression);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
