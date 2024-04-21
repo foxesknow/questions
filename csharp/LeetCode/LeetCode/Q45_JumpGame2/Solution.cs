6b9ae7f747896442da3ab1ddde1b7c5e7485e873
foxesknow <seanbatten58@gmail.com>
2024-04-21 15:25:10 +0100

    Q45 First go

---
+++ csharp/LeetCode/LeetCode/Q45_JumpGame2/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q45_JumpGame2
+{
+    public class Solution
+    {
+        public int Jump(int[] nums) 
+        {
+            return Recurse(nums, 0, int.MaxValue, 0, nums.Length - 1, new());
+        }
+
+        public int Recurse(int[] nums, int acc, int minJump, int index, int lastIndex, Dictionary<int, int> cache)
+        {
+            if(index == lastIndex)
+            {
+                return Math.Min(acc, minJump);
+            }
+
+            if(index > lastIndex)
+            {
+                return minJump;
+            }
+
+            if(cache.TryGetValue(index, out var thisMin))
+            {
+                return thisMin;
+            }
+
+            var jumps = nums[index];
+
+            thisMin = int.MaxValue;
+            for(var i = 1; i <= jumps; i++)
+            {
+                var recurseMin = Recurse(nums, acc + 1, minJump, index + i, lastIndex, cache);
+                thisMin = Math.Min(thisMin, recurseMin);                
+            }
+
+            minJump = Math.Min(minJump, thisMin);
+            cache.Add(index, thisMin);
+
+            return minJump;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q45_JumpGame2/SolutionTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q45_JumpGame2;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q45_JumpGame2
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var steps = s.Jump(new[]{2,3,1,1,4});
+            Assert.That(steps, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var steps = s.Jump(new[]{2,3,0,1,4});
+            Assert.That(steps, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var steps = s.Jump(new[]{5,6,4,4,6,9,4,4,7,4,4,8,2,6,8,1,5,9,6,5,2,7,9,7,9,6,9,4,1,6,8,8,4,4,2,0,3,8,5});
+            Assert.That(steps, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var steps = s.Jump(new[]{1,2,1,1,1});
+            Assert.That(steps, Is.EqualTo(3));
+        }
+    }
+}
