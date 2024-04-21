4caa49dc881c73c5dfdb520d12d855b06b4350ab
foxesknow <seanbatten58@gmail.com>
2024-04-21 14:47:27 +0100

    Question 39

---
+++ csharp/LeetCode/LeetCode/Q39_CombinationSum/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q39_CombinationSum
+{
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target) 
+        {
+            Array.Sort(candidates);
+            List<IList<int>> results = new();
+            Recurse(candidates, target, results, new List<int>(), 0);
+
+            return results;
+        }
+
+        private void Recurse(int[] candidates, int target, IList<IList<int>> results, List<int> acc, int index)
+        {
+            if(target == 0)
+            {
+                results.Add(acc.ToArray());
+                return;
+            }
+
+            for(; index < candidates.Length; index++)
+            {
+                var next = candidates[index];
+                
+                if(next > target)
+                {
+                    // We're done
+                    return;
+                }
+
+                acc.Add(next);
+                Recurse(candidates, target - next, results, acc, index);
+                acc.RemoveAt(acc.Count - 1);
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q39_CombinationSum/SolutionTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q39_CombinationSum;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q39_CombinationSum
+{
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var results = s.CombinationSum(new[]{2,3,6,7}, 7);
+            Assert.That(results, Has.Count.EqualTo(2));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var results = s.CombinationSum(new[]{2,3,5}, 8);
+            Assert.That(results, Has.Count.EqualTo(3));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var results = s.CombinationSum(new[]{2}, 1);
+            Assert.That(results, Has.Count.EqualTo(0));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var results = s.CombinationSum(new[]{2,3,6,7}, 7);
+            Assert.That(results, Has.Count.EqualTo(2));
+        }
+    }
+}
