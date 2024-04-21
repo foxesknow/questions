18972b78da778ea79e1e42b975d002c435fcee55
foxesknow <seanbatten58@gmail.com>
2024-04-21 12:23:39 +0100

    Question 11

---
+++ csharp/LeetCode/LeetCode/Q11_ContainerWithMostWater/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q11_ContainerWithMostWater
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height) 
+        {
+            var max = 0;
+
+            var left = 0;
+            var right = height.Length - 1;
+
+            while(left < right)
+            {
+                var width = right - left;
+                var peak = 0;
+
+                if(height[left] > height[right])
+                {
+                    peak = height[right];
+                    right--;
+                }
+                else
+                {
+                    peak = height[left];
+                    left++;
+                }
+
+                var volumn = width * peak;
+                max = Math.Max(max, volumn);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q11_ContainerWithMostWater/SolutionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q11_ContainerWithMostWater;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q11_ContainerWithMostWater
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var most = s.MaxArea(new[]{1,8,6,2,5,4,8,3,7});
+            Assert.That(most, Is.EqualTo(49));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var most = s.MaxArea(new[]{1,1});
+            Assert.That(most, Is.EqualTo(1));
+        }
+    }
+}
