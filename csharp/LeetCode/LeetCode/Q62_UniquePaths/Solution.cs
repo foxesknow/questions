ff43b5214d582ab74c76a0e39276ef4275d773d4
foxesknow <seanbatten58@gmail.com>
2024-04-22 17:25:27 +0100

    Question 62

---
+++ csharp/LeetCode/LeetCode/Q62_UniquePaths/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q62_UniquePaths
+{
+    public class Solution
+    {
+        public int UniquePaths(int m, int n) 
+        {
+            return Solve(0, 0, m, n, new());
+        }
+
+        private int Solve(int row, int column, int rows, int columns, Dictionary<(int Row, int Column), int> cache)
+        {
+            // We're off the edge of the grid
+            if(row == rows || column == columns) return 0;
+
+            // We're at the bottom right corner (bingo!)
+            if(row == rows - 1 && column == columns - 1) return 1;
+
+            if(cache.TryGetValue((row, column), out var previous))
+            {
+                return previous;
+            }
+
+            var thisCellSolution = Solve(row + 1, column, rows, columns, cache) +
+                                   Solve(row, column + 1, rows, columns, cache);
+            cache.Add((row, column), thisCellSolution);
+
+            return thisCellSolution;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q62_UniquePaths/SolutionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q62_UniquePaths;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q62_UniquePaths
+{
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var paths = s.UniquePaths(3, 7);
+            Assert.That(paths, Is.EqualTo(28));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var paths = s.UniquePaths(3, 2);
+            Assert.That(paths, Is.EqualTo(3));
+        }
+    }
+}
