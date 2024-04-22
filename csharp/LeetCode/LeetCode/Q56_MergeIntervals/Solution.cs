1c757ef8dc685f8c89c7e7f09c7e0d8bdba0012b
foxesknow <seanbatten58@gmail.com>
2024-04-22 16:58:58 +0100

    Question 56

---
+++ csharp/LeetCode/LeetCode/Q56_MergeIntervals/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q56_MergeIntervals
+{
+    public class Solution
+    {
+        public int[][] Merge(int[][] intervals) 
+        {
+            Array.Sort(intervals, (lhs, rhs) => lhs[0].CompareTo(rhs[0]));
+
+            var merged = new List<int[]>();
+
+            merged.Add(intervals[0]);
+
+            for(var i = 1; i < intervals.Length; i++)
+            {
+                var pair = intervals[i];
+                var back = merged[merged.Count - 1];
+                
+                if(pair[0] <= back[1])
+                {
+                    back[1] = Math.Max(back[1], pair[1]);
+                }
+                else
+                {
+                    merged.Add(pair);
+                }
+            }
+
+            return merged.ToArray();
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q56_MergeIntervals/SolutionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q56_MergeIntervals;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q56_MergeIntervals
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            int[][] intervals = [[1,3],[2,6],[8,10],[15,18]];
+            var merged = s.Merge(intervals);
+            Assert.That(merged.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            int[][] intervals = [[1,4],[4,5]];
+            var merged = s.Merge(intervals);
+            Assert.That(merged.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            int[][] intervals = [[1,4],[2,3]];
+            var merged = s.Merge(intervals);
+            Assert.That(merged.Count, Is.EqualTo(1));
+        }
+    }
+}
