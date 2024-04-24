bbf7b5fc746aa05308e973545bb451310d53962b
foxesknow <seanbatten58@gmail.com>
2024-04-24 09:11:43 +0100

    Question 74

---
+++ csharp/LeetCode/LeetCode/Q74_Search2DMatrix/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q74_Search2DMatrix
+{
+    public class Solution
+    {
+        public bool SearchMatrix(int[][] matrix, int target) 
+        {
+            var rows = matrix.Length;
+            var columns = matrix[0].Length;
+
+            var left = 0;
+            var right = (rows * columns) - 1;
+
+            while(left <= right)
+            {
+                var mid = (left + right) / 2;
+                
+                var value = GetValue(matrix, columns, mid);
+                if(value == target) return true;
+
+                if(value < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return false;
+        }
+
+        private int GetValue(int[][] matrix, int numberOfColumns, int index)
+        {
+            var row = index / numberOfColumns;
+            var col = index % numberOfColumns;
+            var value = matrix[row][col];
+            
+            return value;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q74_Search2DMatrix/SolutionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q74_Search2DMatrix;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q74_Search2DMatrix
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase(3, true)]
+        [TestCase(13, false)]
+        [TestCase(0, false)]
+        [TestCase(999, false)]
+        [TestCase(1, true)]
+        [TestCase(60, true)]
+        public void Example1(int target, bool expected)
+        {
+            var s = new Solution();
+            var matrix = new int[][]
+            {
+                new[]{1,3,5,7},
+                new[]{10,11,16,20},
+                new[]{23,30,34,60}
+            };
+
+            Assert.That(s.SearchMatrix(matrix, target), Is.EqualTo(expected));
+        }
+    }
+}
