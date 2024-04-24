ae5e3617c06bf10906eb43f53b331e1cdb6a5446
foxesknow <seanbatten58@gmail.com>
2024-04-24 18:17:14 +0100

    CountingTriangles

---
+++ csharp/LeetCode/LeetCode/CountingTriangles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class CountingTriangles
+    {
+        public int CountDistinct(IList<int[]> arr)
+        {
+            var unique = new HashSet<(int, int, int)>();
+
+            foreach(var triangle in arr)
+            {
+                // Sort it;
+                var (a, b, c) = (triangle[0], triangle[1], triangle[2]);
+                SwapIfGreater(ref a, ref b);
+                SwapIfGreater(ref b, ref c);
+                SwapIfGreater(ref a, ref b);
+
+                unique.Add((a, b, c));
+            }
+
+            return unique.Count;;
+        }
+
+        private void SwapIfGreater(ref int a, ref int b)
+        {
+            if(a > b)
+            {
+                var temp = a;
+                a = b;
+                b = temp;
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/CountingTrianglesTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class CountingTrianglesTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new CountingTriangles();
+            var triangles = new List<int[]>
+            {
+                new[]{2,2,3},
+                new[]{3,2,2},
+                new[]{2,5,6}
+            };
+
+            Assert.That(s.CountDistinct(triangles), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new CountingTriangles();
+            var triangles = new List<int[]>
+            {
+                 new[]{8, 4, 6},
+                 new[]{100, 101, 102},
+                 new[]{84, 93, 173}
+            };
+
+            Assert.That(s.CountDistinct(triangles), Is.EqualTo(3));
+        }
+    }
+}
