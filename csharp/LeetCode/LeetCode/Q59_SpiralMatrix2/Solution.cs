457496dc9ba961e462a7891c7a922bbe8e03e289
foxesknow <seanbatten58@gmail.com>
2024-04-26 18:04:57 +0100

    Question 59

---
+++ csharp/LeetCode/LeetCode/Q59_SpiralMatrix2/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q59_SpiralMatrix2
+{
+    public class Solution
+    {
+        public int[][] GenerateMatrix(int n) 
+        {
+            var matrix = new int[n][];
+            for(var i = 0; i < n; i++)
+            {
+                matrix[i] = new int[n];
+            }
+
+            var totalItems = n * n;
+            int col = 0, colStart = 0, colStop = n - 1, colDirection = 1;
+            int row = 0, rowStart = 0, rowStop = n - 1, rowDirection = 0;
+
+            for(int i = 0; i < totalItems; i++)
+            {
+                matrix[row][col] = i + 1;
+
+                if(colDirection == 1 && col == colStop)
+                {
+                    rowDirection = 1;
+                    colDirection = 0;
+                    rowStart++;
+                }
+                else if(colDirection == -1 && col == colStart)
+                {
+                    rowDirection = -1;
+                    colDirection = 0;
+                    rowStop--;
+                }
+                else if(rowDirection == 1 && row == rowStop)
+                {
+                    rowDirection = 0;
+                    colDirection = -1;
+                    colStop--;
+                }
+                else if(rowDirection == -1 && row == rowStart)
+                {
+                    rowDirection = 0;
+                    colDirection = 1;
+                    colStart++;
+                }
+
+                row += rowDirection;
+                col += colDirection;
+            }
+
+            return matrix;            
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q59_SpiralMatrix2/SolutionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q59_SpiralMatrix2;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q59_SpiralMatrix2
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var matrix = s.GenerateMatrix(3);
+            Assert.That(matrix[0], Is.EqualTo(new[]{1,2,3}));
+            Assert.That(matrix[1], Is.EqualTo(new[]{8,9,4}));
+            Assert.That(matrix[2], Is.EqualTo(new[]{7,6,5}));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var matrix = s.GenerateMatrix(1);
+            Assert.That(matrix[0], Is.EqualTo(new[]{1}));
+        }
+    }
+}
