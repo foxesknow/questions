f6ac93e48bece472a888ea2c75610e38677d2e55
foxesknow <seanbatten58@gmail.com>
2024-04-26 19:14:02 +0100

    Question 71

---
+++ csharp/LeetCode/LeetCode/Q71_SimplifyPath/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q71_SimplifyPath
+{
+    public class Solution
+    {
+        public string SimplifyPath(string path) 
+        {
+            var parts = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            var stack = new Stack<string>(parts.Length);
+
+            foreach(var part in parts)
+            {
+                if(part == ".")
+                {
+                    continue;
+                }
+                else if(part == "..")
+                {
+                    stack.TryPop(out var _);
+                }
+                else
+                {
+                    stack.Push(part);
+                }
+            }
+
+            var normalizedPath = "/" + string.Join('/', stack.Reverse());
+            return normalizedPath;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q71_SimplifyPath/SolutionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q71_SimplifyPath;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q71_SimplifyPath
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("/home/", "/home")]
+        [TestCase("/../", "/")]
+        [TestCase("/home//foo/", "/home/foo")]
+        public void Examples(string path, string expected)
+        {
+            var s = new Solution();
+            Assert.That(s.SimplifyPath(path), Is.EqualTo(expected));
+        }
+    }
+}
