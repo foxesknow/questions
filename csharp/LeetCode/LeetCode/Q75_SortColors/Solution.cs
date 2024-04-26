3bfa3530ce78a8f32bc2b7424195effdcf875731
foxesknow <seanbatten58@gmail.com>
2024-04-26 19:38:23 +0100

    Question 75

---
+++ csharp/LeetCode/LeetCode/Q75_SortColors/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q75_SortColors
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums) 
+        {
+            Span<int> counts = stackalloc int[3];
+
+            foreach(var n in nums)
+            {
+                counts[n]++;
+            }
+
+            Array.Fill(nums, 0, 0, counts[0]);
+            Array.Fill(nums, 1, counts[0], counts[1]);
+            Array.Fill(nums, 2, counts[0] + counts[1], counts[2]);
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q75_SortColors/SolutionTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q75_SortColors;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q75_SortColors
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var colors = new int[]{2,0,2,1,1,0};
+            s.SortColors(colors);
+            Assert.That(colors, Is.EqualTo(new int[]{0,0,1,1,2,2,}));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var colors = new int[]{2,0,1};
+            s.SortColors(colors);
+            Assert.That(colors, Is.EqualTo(new int[]{0,1,2}));
+        }
+    }
+}
