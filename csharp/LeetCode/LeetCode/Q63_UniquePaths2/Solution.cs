262db275160083ffebac1dddd2cab6550d4e5178
foxesknow <seanbatten58@gmail.com>
2024-04-26 18:27:19 +0100

    Question 63

---
+++ csharp/LeetCode/LeetCode/Q63_UniquePaths2/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q63_UniquePaths2
+{
+    public class Solution
+    {
+        public int UniquePathsWithObstacles(int[][] obstacleGrid) 
+        {
+            return Solve(0, 0, obstacleGrid.Length, obstacleGrid[0].Length, new(), obstacleGrid);
+        }
+
+        private int Solve(int row, int column, int rows, int columns, Dictionary<(int Row, int Column), int> cache, int[][] obstacleGrid)
+        {
+            // We're off the edge of the grid
+            if(row == rows || column == columns) return 0;
+
+            // We're at the bottom right corner (bingo!)
+            if(row == rows - 1 && column == columns - 1) 
+            {
+                return obstacleGrid[row][column] == 1 ? 0 : 1;
+            }
+
+            if(obstacleGrid[row][column] == 1) return 0;
+
+            if(cache.TryGetValue((row, column), out var previous))
+            {
+                return previous;
+            }
+
+            var thisCellSolution = Solve(row + 1, column, rows, columns, cache, obstacleGrid) +
+                                   Solve(row, column + 1, rows, columns, cache, obstacleGrid);
+            cache.Add((row, column), thisCellSolution);
+
+            return thisCellSolution;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q63_UniquePaths2/SolutionTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q63_UniquePaths2;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q63_UniquePaths2
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var matrix = new int[][]
+            {
+                new[]{0,0,0},
+                new[]{0,1,0},
+                new[]{0,0,0},
+            };
+
+            Assert.That(s.UniquePathsWithObstacles(matrix), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var matrix = new int[][]
+            {
+                new[]{0,1},
+                new[]{0,0},
+            };
+
+            Assert.That(s.UniquePathsWithObstacles(matrix), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var matrix = new int[][]
+            {
+                new[]{0,0,0},
+                new[]{0,0,1},
+                new[]{0,1,0},
+            };
+
+            Assert.That(s.UniquePathsWithObstacles(matrix), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var matrix = new int[][]
+            {
+                new[]{0,0,0},
+                new[]{0,0,0},
+                new[]{0,0,1},
+            };
+
+            Assert.That(s.UniquePathsWithObstacles(matrix), Is.EqualTo(0));
+        }
+    }
+}
