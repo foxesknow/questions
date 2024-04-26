6d1099a3c893b09205a09e63723869448afafc98
foxesknow <seanbatten58@gmail.com>
2024-04-26 13:16:00 +0100

    Q283

---
+++ csharp/LeetCode/LeetCode/Q283_MoveZeros/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q283_MoveZeros
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums) 
+        {
+            var insertionPoint = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != 0)
+                {
+                    nums[insertionPoint] = nums[i];
+                    insertionPoint++;
+                }
+            }
+
+            Array.Fill(nums, 0, insertionPoint, nums.Length - insertionPoint);
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q283_MoveZeros/SolutionTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q283_MoveZeros;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q283_MoveZeros
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var numbers = new[]{0,1,0,3,12};
+            s.MoveZeroes(numbers);
+            Assert.That(numbers, Is.EqualTo(new[]{1,3,12,0,0}));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var numbers = new[]{0};
+            s.MoveZeroes(numbers);
+            Assert.That(numbers, Is.EqualTo(new[]{0}));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var numbers = new[]{8};
+            s.MoveZeroes(numbers);
+            Assert.That(numbers, Is.EqualTo(new[]{8}));
+        }
+    }
+}
