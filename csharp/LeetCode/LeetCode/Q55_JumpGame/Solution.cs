b287676f38498441d34b3e86341d774bd944f33d
foxesknow <seanbatten58@gmail.com>
2024-04-26 17:51:56 +0100

    Question 55

---
+++ csharp/LeetCode/LeetCode/Q55_JumpGame/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q55_JumpGame
+{
+    public class Solution
+    {
+        public bool CanJump(int[] nums) 
+        {
+            var energy = 0;
+
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                if(nums[i] > energy)
+                {
+                    energy = nums[i];
+                }
+
+                energy--;
+
+                if(energy < 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q55_JumpGame/SolutionTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q55_JumpGame;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q55_JumpGame
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var madeIt = s.CanJump(new[]{2,3,1,1,4});
+            Assert.That(madeIt, Is.True);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var madeIt = s.CanJump(new[]{3,2,1,0,4});
+            Assert.That(madeIt, Is.False);
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var madeIt = s.CanJump(new[]{3,2,1,1,4});
+            Assert.That(madeIt, Is.True);
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var madeIt = s.CanJump(new[]{0});
+            Assert.That(madeIt, Is.True);
+        }
+
+        [Test]
+        public void Example5()
+        {
+            var s = new Solution();
+            var madeIt = s.CanJump(new[]{2,0,0});
+            Assert.That(madeIt, Is.True);
+        }
+    }
+}
