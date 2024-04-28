6534229b7cc9c11dedf434daa822f58f7ee67e6f
foxesknow <seanbatten58@gmail.com>
2024-04-28 18:06:48 +0100

    Question 41

---
+++ csharp/LeetCode/LeetCode/Q41_FirstMissingPositive/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q41_FirstMissingPositive
+{
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums) 
+        {
+            var n = nums.Length;
+
+            for(var i = 0; i < n; i++)
+            {
+                if(nums[i] <= 0 || nums[i] > n)
+                {
+                    nums[i] = n + 1;
+                }
+            }
+
+            // Now mark all the array items that are reachable
+            // We'll negate the value in the array item to indicate we've reached it
+            for(int i = 0; i < n; i++)
+            {
+                // If the index is beyond the array then don't bother
+                if(Math.Abs(nums[i]) > n) continue;
+
+                // Only mark the index if we've not already seen it
+                var index = Math.Abs(nums[i]) - 1; // -1 as array is 0 based
+                if(nums[index] > 0)
+                {
+                    nums[index] *= -1;
+                }
+            }
+
+            // Now just walk the array until we get to the first
+            // item that we never reached
+            for(int i = 0; i < n; i++)
+            {
+                if(nums[i] > 0) return i + 1;
+            }
+
+            return n + 1;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q41_FirstMissingPositive/SolutionTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q41_FirstMissingPositive;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q41_FirstMissingPositive
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{1,2,0});
+            Assert.That(first, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{3,4,-1,1});
+            Assert.That(first, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{7,8,9,11,12});
+            Assert.That(first, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{1,1});
+            Assert.That(first, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example5()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{1,1000});
+            Assert.That(first, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example6()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{1,2,3,4,5,6,7,8,9,10,11,12,23,20});
+            Assert.That(first, Is.EqualTo(13));
+        }
+
+        [Test]
+        public void Example7()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{1,1,1,3});
+            Assert.That(first, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example8()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{1});
+            Assert.That(first, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example9()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{1,3,3});
+            Assert.That(first, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example10()
+        {
+            var s = new Solution();
+            var first = s.FirstMissingPositive(new[]{2,2,4,0,1,3,3,3,4,3});
+            Assert.That(first, Is.EqualTo(5));
+        }
+    }
+}
