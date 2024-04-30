c2f6f8c431982a8894363c8416b8d9bdf805152d
foxesknow <seanbatten58@gmail.com>
2024-04-30 15:23:08 +0100

    Q146

---
+++ csharp/LeetCode/LeetCode/Q146_LRUCache/LRUCache.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q146_LRUCache
+{
+    public class LRUCache
+    {
+        private readonly LinkedList<(int Key, int Value)> m_Lru = new();
+        private readonly Dictionary<int, LinkedListNode<(int Key, int Value)>> m_Cache= new();
+        private readonly int m_Capacity;
+
+        public LRUCache(int capacity) 
+        {
+            m_Capacity = capacity;
+        }
+    
+        public int Get(int key) 
+        {
+            if(m_Cache.TryGetValue(key, out var node))
+            {
+                // It's now our most recently used item!
+                if(node != m_Lru.First)
+                {
+                    m_Lru.Remove(node);
+                    m_Lru.AddFirst(node);
+                }
+
+                return node.Value.Value;
+            }
+            
+            return -1;
+        }
+    
+        public void Put(int key, int value) 
+        {
+            if(m_Cache.TryGetValue(key, out var node))
+            {
+                // It's now our most recently used item!
+                if(node != m_Lru.First)
+                {
+                    m_Lru.Remove(node);
+                    m_Lru.AddFirst(node);
+                }  
+                
+                node.Value = (key, value);
+            }
+            else
+            {
+                node = m_Lru.AddFirst((key, value));
+                m_Cache.Add(key, node);
+
+                if(m_Cache.Count > m_Capacity)
+                {
+                    var last = m_Lru.Last;
+                    m_Lru.RemoveLast();
+                    m_Cache.Remove(last.Value.Key);
+                }
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q146_LRUCache/LRUCacheTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q146_LRUCache;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q146_LRUCache
+{
+    [TestFixture]
+    public class LRUCacheTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var lru = new LRUCache(2);
+            lru.Put(1, 1);
+            lru.Put(2, 2);
+            Assert.That(lru.Get(1), Is.EqualTo(1));
+
+            lru.Put(3, 3);
+            Assert.That(lru.Get(2), Is.EqualTo(-1));
+
+            lru.Put(4, 4);
+            Assert.That(lru.Get(1), Is.EqualTo(-1));
+            Assert.That(lru.Get(3), Is.EqualTo(3));
+            Assert.That(lru.Get(4), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var lru = new LRUCache(1);
+            lru.Put(2, 1);
+            Assert.That(lru.Get(2), Is.EqualTo(1));
+        }
+    }
+}
