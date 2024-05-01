57ef82267c41992a76d24fc18365f6ff193c7957
foxesknow <seanbatten58@gmail.com>
2024-05-01 09:53:18 +0100

    Question 121

---
+++ csharp/LeetCode/LeetCode/Q121_BestTimeToBuySellStock/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q121_BestTimeToBuySellStock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices) 
+        {
+            var profit = 0;
+            var minPrice = prices[0];
+
+            for(var i = 1; i < prices.Length; i++)
+            {
+                var thisProfit = prices[i] - minPrice;
+                profit = Math.Max(profit, thisProfit);
+                minPrice = Math.Min(minPrice, prices[i]);
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q121_BestTimeToBuySellStock/SolutionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q121_BestTimeToBuySellStock;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q121_BestTimeToBuySellStock
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            Assert.That(s.MaxProfit(new int[]{7,1,5,3,6,4}), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            Assert.That(s.MaxProfit(new int[]{7,6,4,3,1}), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            Assert.That(s.MaxProfit(new int[]{1,2,3,4,5,6}), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            Assert.That(s.MaxProfit(new int[]{7}), Is.EqualTo(0));
+        }
+    }
+}
