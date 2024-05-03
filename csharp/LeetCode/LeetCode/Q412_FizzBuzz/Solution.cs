c9a7b817b8feab60ad67227ce1c93b279d56c212
foxesknow <seanbatten58@gmail.com>
2024-05-03 15:39:25 +0100

    Question 412

---
+++ csharp/LeetCode/LeetCode/Q412_FizzBuzz/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q412_FizzBuzz
+{
+    public class Solution
+    {
+        public IList<string> FizzBuzz(int n) 
+        {
+            var results = new string[n];
+
+            for(int i = 1; i <= n; i++)
+            {
+                var s = i switch
+                {
+                    var x when x % 15 == 0  => "FizzBuzz",
+                    var x when x % 5 == 0   => "Buzz",
+                    var x when x % 3 == 0   => "Fizz",
+                    var x                   => x.ToString()
+                };
+
+                results[i - 1] = s;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q412_FizzBuzz/SolutionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q412_FizzBuzz;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q412_FizzBuzz
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var results = s.FizzBuzz(3);
+            Assert.That(results, Is.EqualTo(new[]{"1", "2", "Fizz"}));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var results = s.FizzBuzz(5);
+            Assert.That(results, Is.EqualTo(new[]{"1", "2", "Fizz", "4", "Buzz"}));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var results = s.FizzBuzz(15);
+            Assert.That(results, Is.EqualTo(new[]{"1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"}));
+        }
+    }
+}
