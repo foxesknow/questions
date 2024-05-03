74673e04e69f6d80774b79999cbc7dbef0fb4fb5
foxesknow <seanbatten58@gmail.com>
2024-05-03 16:49:49 +0100

    Question 415

---
+++ csharp/LeetCode/LeetCode/Q415_AddStrings/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q415_AddStrings
+{
+    public class Solution
+    {
+        public string AddStrings(string num1, string num2) 
+        {
+            var reverseSum = ReverseSum(num1, num2);
+            return new string(reverseSum.Reverse().ToArray());
+
+        }
+
+        private IEnumerable<char> ReverseSum(string num1, string num2) 
+        {
+            var maxLength = Math.Max(num1.Length, num2.Length);
+            var builder = new StringBuilder(maxLength + 1);
+
+            int carry = 0;
+            var zip = Enumerable.Zip(ReverseScan(num1, maxLength), ReverseScan(num2, maxLength));
+            
+            foreach(var (lhs, rhs) in zip)
+            {
+                var sum = lhs + rhs + carry;
+                carry = sum / 10;
+                char digit = (char)('0' + (sum % 10));
+                yield return digit;
+            }
+
+            if(carry != 0) yield return '1';
+        }
+
+        public IEnumerable<int> ReverseScan(string num, int count)
+        {
+            for(var i = num.Length - 1; i >= 0; i--)
+            {
+                yield return num[i] - '0';
+                count--;
+            }
+
+            while(count-- > 0)
+            {
+                yield return 0;
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q415_AddStrings/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q415_AddStrings;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q415_AddStrings
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("11", "123", "134")]
+        [TestCase("1", "12345", "12346")]
+        public void Examples(string num1, string num2, string expected)
+        {
+            var s = new Solution();
+            Assert.That(s.AddStrings(num1, num2), Is.EqualTo(expected));
+        }
+    }
+}
