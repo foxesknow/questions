4377759f8e1fc571fb94e15df6f306197ab2f65a
foxesknow <seanbatten58@gmail.com>
2024-05-03 15:11:38 +0100

    Question 226

---
+++ csharp/LeetCode/LeetCode/Q226_InvertBinaryTree/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q226_InvertBinaryTree
+{
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root) 
+        {
+            if(root is null) return root;
+
+            var newLeft = InvertTree(root.right);
+            var newRight = InvertTree(root.left);
+
+            root.left = newLeft;
+            root.right = newRight;
+
+            return root;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q226_InvertBinaryTree/SolutionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using LeetCode.Q226_InvertBinaryTree;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q226_InvertBinaryTree
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s= new Solution();
+            var root = new TreeNode
+            (
+                2,
+                new TreeNode(1),
+                new TreeNode(3)
+            );
+
+            var inverted = s.InvertTree(root);
+            Assert.That(inverted.val, Is.EqualTo(2));
+            Assert.That(inverted.left.val, Is.EqualTo(3));
+            Assert.That(inverted.right.val, Is.EqualTo(1));
+        }
+    }
+}
