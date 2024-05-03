bc832d5fc018333dcc449ae2e673eaf5637f238b
foxesknow <seanbatten58@gmail.com>
2024-05-03 14:59:14 +0100

    Question 191

---
+++ csharp/LeetCode/LeetCode/Q191_NumberOfOneBits/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q191_NumberOfOneBits
+{
+    public class Solution
+    {
+        public int HammingWeight(int n) 
+        {
+            var count = 0;
+
+            while(n != 0)
+            {
+                n &= (n - 1);
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q191_NumberOfOneBits/SolutionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q191_NumberOfOneBits;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q191_NumberOfOneBits
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase(11, 3)]
+        [TestCase(128, 1)]
+        [TestCase(0, 0)]
+        [TestCase(2147483645, 30)]
+        [TestCase(int.MaxValue, 31)]
+        public void Examples(int number, int expected)
+        {
+            var s = new Solution();
+            Assert.That(s.HammingWeight(number), Is.EqualTo(expected));
+        }
+    }
+}
