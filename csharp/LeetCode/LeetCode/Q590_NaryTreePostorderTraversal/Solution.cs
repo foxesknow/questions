f1c85195c53431e30790b6779eb0128d2d2d7964
foxesknow <seanbatten58@gmail.com>
2024-05-03 17:16:56 +0100

    Question 590

---
+++ csharp/LeetCode/LeetCode/Q590_NaryTreePostorderTraversal/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q590_NaryTreePostorderTraversal
+{
+    public class Solution
+    {
+        public IList<int> Postorder(Node root) 
+        {
+            var results = new List<int>();
+            Recurse(root, results);
+
+            return results;
+        }
+
+        private void Recurse(Node root, List<int> accumulator)
+        {
+            if(root is null) return;
+
+            if(root.children is not null)
+            {
+                for(int i = 0; i < root.children.Count; i++)
+                {
+                    Recurse(root.children[i], accumulator);
+                }
+            }
+
+            accumulator.Add(root.val);
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+}
