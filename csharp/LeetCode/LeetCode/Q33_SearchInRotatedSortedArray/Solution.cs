8fbb5c6e661321a1f4c401a6ba113297587cd7dc
foxesknow <seanbatten58@gmail.com>
2024-05-05 17:48:52 +0100

    Question 33

---
+++ csharp/LeetCode/LeetCode/Q33_SearchInRotatedSortedArray/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q33_SearchInRotatedSortedArray
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target) 
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while(left <= right)
+            {
+                var mid = (left + right) / 2;
+                
+                if(nums[mid] == target) return mid;
+
+                if(nums[left] <= nums[mid])
+                {
+                    // The left is sorted
+                    if(target >= nums[left] && target < nums[mid])
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else
+                {
+                    // The right is sorted
+                    if(target > nums[mid] && target <= nums[right])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q33_SearchInRotatedSortedArray/SolutionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q33_SearchInRotatedSortedArray;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q33_SearchInRotatedSortedArray
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase(4, 0)]
+        [TestCase(5, 1)]
+        [TestCase(6, 2)]
+        [TestCase(7, 3)]
+        [TestCase(0, 4)]
+        [TestCase(1, 5)]
+        [TestCase(2, 6)]
+        [TestCase(-10, -1)]
+        [TestCase(100, -1)]
+        public void Example1(int value, int expectedIndex)
+        {
+            var s = new Solution();
+            var numbers = new int[]{4,5,6,7,0,1,2};
+            Assert.That(s.Search(numbers, value), Is.EqualTo(expectedIndex));
+        }
+    }
+}
