17bfc13ed0bb84c20602e856d692fe17f6a2f92a
foxesknow <seanbatten58@gmail.com>
2024-05-05 18:08:27 +0100

    Question 80

---
+++ csharp/LeetCode/LeetCode/Q80_RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q80_RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums) 
+        {
+            var length = nums.Length;
+            if(length <= 2) return length;
+
+            var insertionPoint = 1;
+            var current = nums[0];
+            var currentCount = 1;
+            var count = 1;
+
+            for(var i = 1; i < length; i++)
+            {
+                if(nums[i] == current)
+                {
+                    if(currentCount == 1)
+                    {
+                        nums[insertionPoint] = nums[i];
+                        insertionPoint++;
+                        count++;
+                    }
+
+                    currentCount++;
+                }
+                else
+                {
+                    // It's a new number
+                    nums[insertionPoint] = nums[i];
+                    insertionPoint++;
+                    currentCount = 1;
+                    current = nums[i];
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q80_RemoveDuplicatesFromSortedArray/SolutionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q80_RemoveDuplicatesFromSortedArray;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q80_RemoveDuplicatesFromSortedArray
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var nums = new int[]{1,1,1,2,2,3};
+            Assert.That(s.RemoveDuplicates(nums), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var nums = new int[]{0,0,1,1,1,1,2,3,3};
+            Assert.That(s.RemoveDuplicates(nums), Is.EqualTo(7));
+        }
+    }
+}
