e6cac3a0fcb3aceacdaef7b99b8070682d2e39c3
foxesknow <seanbatten58@gmail.com>
2024-05-06 15:05:58 +0100

    Question 14

---
+++ csharp/LeetCode/LeetCode/Q14_LongestCommonPrefix/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q14_LongestCommonPrefix
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs) 
+        {
+            if(strs.Length == 1) return strs[0];
+
+            var (length, referenceString) = strs.Min(s => (s.Length, s));
+            var builder = new StringBuilder(length);
+
+            for(var i = 0; i < length; i++)
+            {
+                var c = referenceString[i];
+                
+                var allSame = strs.All(s => s[i] == c);
+                if(allSame)
+                {
+                    builder.Append(c);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q14_LongestCommonPrefix/SolutionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q14_LongestCommonPrefix;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q14_LongestCommonPrefix
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var strs = new string[]{"flower","flow","flight"};
+            Assert.That(s.LongestCommonPrefix(strs), Is.EqualTo("fl"));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var strs = new string[]{"dog","racecar","car"};
+            Assert.That(s.LongestCommonPrefix(strs), Is.EqualTo(""));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var strs = new string[]{"dog","dog","dog"};
+            Assert.That(s.LongestCommonPrefix(strs), Is.EqualTo("dog"));
+        }
+    }
+}
