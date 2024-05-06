cf31272df608d6c96d469225f633f0546a832f60
foxesknow <seanbatten58@gmail.com>
2024-05-06 17:07:52 +0100

    Question 13

---
+++ csharp/LeetCode/LeetCode/Q13_RomanToInteger/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q13_RomanToInteger
+{
+    public class Solution
+    {
+        public int RomanToInt(string s) 
+        {
+            var value = 0;
+            char previous = '\0';
+
+            foreach(var c in s)
+            {
+                var adjustment = (previous, c) switch
+                {
+                    ('C', 'M') => 800,
+                    ('C', 'D') => 300,
+                    ('X', 'L') => 30,
+                    ('X', 'C') => 80,
+                    ('I', 'X') => 8,
+                    ('I', 'V') => 3,
+                    (_, 'M') => 1000,
+                    (_, 'D') => 500,
+                    (_, 'C') => 100,
+                    (_, 'L') => 50,
+                    (_, 'X') => 10,
+                    (_, 'V') => 5,
+                    (_, 'I') => 1,
+                    _ => throw new Exception()
+                };
+
+                value += adjustment;
+                previous = c;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q13_RomanToInteger/SolutionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q13_RomanToInteger;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q13_RomanToInteger
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("III", 3)]
+        [TestCase("LVIII", 58)]
+        [TestCase("MCMXCIV", 1994)]
+        public void Examples(string roman, int expected)
+        {
+            var s = new Solution();
+            Assert.That(s.RomanToInt(roman), Is.EqualTo(expected));
+        }
+    }
+}
