64e7d39e7f84e8c5908abcb0b2ea0357d8dc2bb9
foxesknow <seanbatten58@gmail.com>
2024-05-07 17:45:44 +0100

    Question 53

---
+++ csharp/LeetCode/LeetCode/Q53_MaxSubArray/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q53_MaxSubArray
+{
+    public class Solution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            var max = nums[0];
+            var sum = nums[0];
+
+            for(var i = 1; i < nums.Length; i++)
+            {
+                var current = nums[i];
+                sum = Math.Max(current, sum + current);
+                max = Math.Max(max, sum);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q53_MaxSubArray/SolutionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q53_MaxSubArray;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q53_MaxSubArray
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+            Assert.That(s.MaxSubArray(nums), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var nums = new int[]{5,4,-1,7,8};
+            Assert.That(s.MaxSubArray(nums), Is.EqualTo(23));
+        }
+    }
+}
