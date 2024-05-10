0a9f32d93eb0e71759bb448cb59c6a0dc3c8cb2d
foxesknow <seanbatten58@gmail.com>
2024-05-10 15:45:45 +0100

    Question 234

---
+++ csharp/LeetCode/LeetCode/Q234_PalindromeLinkedList/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q234_PalindromeLinkedList
+{
+    public class Solution
+    {
+        public bool IsPalindrome(ListNode head) 
+        {
+            var length = Length(head);
+            if(length == 1) return true;
+
+            var halfWay = length / 2;
+
+            // Reverse the first part of the list;
+            ListNode reversed = null;
+            for(int i = 0; i < halfWay; i++)
+            {
+                var next = head.next;
+                head.next = reversed;
+                reversed = head;
+                head = next;
+            }
+
+            // If the length is odd then we can ignore the first item of the head
+            if((length & 1) == 1) head = head.next;
+
+            for(var i = 0; i < halfWay; i++)
+            {
+                if(reversed.val != head.val) return false;
+
+                reversed = reversed.next;
+                head = head.next;
+            }
+
+            return true;
+        }
+
+        private int Length(ListNode node)
+        {
+            var length = 0;
+
+            while(node is not null)
+            {
+                length++;
+                node = node.next;
+            }
+
+            return length;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q234_PalindromeLinkedList/SolutionTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using LeetCode.Q234_PalindromeLinkedList;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q234_PalindromeLinkedList
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 2, 1);
+            Assert.That(s.IsPalindrome(list), Is.True);
+        }
+
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2);
+            Assert.That(s.IsPalindrome(list), Is.False);
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 4, 2, 1);
+            Assert.That(s.IsPalindrome(list), Is.True);
+        }
+    }
+}
