6da21202256b20f4f6fcbc8ebabf3f0c45bc573a
foxesknow <seanbatten58@gmail.com>
2024-05-10 15:25:19 +0100

    Question 242

---
+++ csharp/LeetCode/LeetCode/Q242_ValidAnagram/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q242_ValidAnagram
+{
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t) 
+        {
+            if(s.Length != t.Length) return false;
+
+            Span<int> cache = stackalloc int[26];
+
+            var length = s.Length;
+            for(var i = 0; i < s.Length; i++)
+            {
+                cache[s[i] - 'a']++;
+                cache[t[i] - 'a']--;
+            }
+
+            for(var i = 0; i < cache.Length; i++)
+            {
+                if(cache[i] < 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q242_ValidAnagram/SolutionTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q242_ValidAnagram;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q242_ValidAnagram
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("anagram", "nagaram", true)]
+        [TestCase("rat", "car", false)]
+        public void Examples(string s, string t, bool expected)
+        {
+            var solution = new Solution();
+            Assert.That(solution.IsAnagram(s, t), Is.EqualTo(expected));
+        }
+    }
+}
