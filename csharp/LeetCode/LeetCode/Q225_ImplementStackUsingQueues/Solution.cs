76e5c20ebbfba79295d94c10169accb001d1e29b
foxesknow <seanbatten58@gmail.com>
2024-05-10 15:14:01 +0100

    Question 225

---
+++ csharp/LeetCode/LeetCode/Q225_ImplementStackUsingQueues/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q225_ImplementStackUsingQueues
+{
+    public class MyStack
+    {
+        private readonly Queue<int> m_Queue = new();
+
+        public MyStack()
+        {
+
+        }
+
+        public void Push(int x)
+        {
+            var shuffleCount = m_Queue.Count;
+            m_Queue.Enqueue(x);
+
+            for(var i = 0; i < shuffleCount; i++)
+            {
+                var item = m_Queue.Dequeue();
+                m_Queue.Enqueue(item);
+            }
+        }
+
+        public int Pop()
+        {
+            return m_Queue.Dequeue();
+        }
+
+        public int Top()
+        {
+            return m_Queue.Peek();
+        }
+
+        public bool Empty()
+        {
+            return m_Queue.Count == 0;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q225_ImplementStackUsingQueues/SolutionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q225_ImplementStackUsingQueues;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q225_ImplementStackUsingQueues
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var stack = new MyStack();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.That(stack.Top(), Is.EqualTo(2));
+            Assert.That(stack.Pop(), Is.EqualTo(2));
+            Assert.That(stack.Empty(), Is.False);
+        }
+    }
+}
