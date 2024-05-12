6e2c23ff317c4adaf7a4d11d9bcf6c8ec7124e42
foxesknow <seanbatten58@gmail.com>
2024-05-12 15:28:23 +0100

    Question 4

---
+++ csharp/LeetCode/LeetCode/Q4_MedianOfTwoSortedArrays/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q4_MedianOfTwoSortedArrays
+{
+    public class Solution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2) 
+        {
+            var totalLength = nums1.Length + nums2.Length;
+            var medianPosition = (int)Math.Ceiling(totalLength / 2d);
+            bool isEven = (totalLength % 2) == 0;            
+
+            Span<int> s1 = nums1;
+            Span<int> s2 = nums2;
+
+            double median = 0;
+
+            for(var i = 0; i < medianPosition; i++)
+            {
+                median = Advance(ref s1, ref s2);
+            }
+
+            if(isEven)
+            {
+                var next = Advance(ref s1, ref s2);
+                median = (median + next) / 2;
+            }
+
+            return median;
+        }
+
+        private int Advance(ref Span<int> nums1, ref Span<int> nums2)
+        {
+            if(nums1.Length == 0)
+            {
+                var value = nums2[0];
+                nums2 = nums2[1..];
+
+                return value;
+            }
+
+            if(nums2.Length == 0)
+            {
+                var value = nums1[0];
+                nums1 = nums1[1..];
+
+                return value;
+            }
+
+            if(nums1[0] <= nums2[0])
+            {
+                var value = nums1[0];
+                nums1 = nums1[1..];
+
+                return value;
+            }
+            else
+            {
+                var value = nums2[0];
+                nums2 = nums2[1..];
+
+                return value;
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q4_MedianOfTwoSortedArrays/SolutionTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q4_MedianOfTwoSortedArrays;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q4_MedianOfTwoSortedArrays
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var nums1 = new int[]{1, 3};
+            var nums2 = new int[]{2};
+            Assert.That(s.FindMedianSortedArrays(nums1, nums2), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var nums1 = new int[]{1, 2};
+            var nums2 = new int[]{3,4};
+            Assert.That(s.FindMedianSortedArrays(nums1, nums2), Is.EqualTo(2.5));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var nums1 = new int[]{};
+            var nums2 = new int[]{3,4};
+            Assert.That(s.FindMedianSortedArrays(nums1, nums2), Is.EqualTo(3.5));
+        }
+    }
+}
