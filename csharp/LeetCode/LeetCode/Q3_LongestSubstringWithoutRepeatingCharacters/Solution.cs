2467685984810290b87d1348a6d2c0e0b8d4ba26
foxesknow <seanbatten58@gmail.com>
2024-05-12 16:25:24 +0100

    Question 3

---
+++ csharp/LeetCode/LeetCode/Q3_LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q3_LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s) 
+        {
+            if(s.Length == 0) return 0;
+
+            Span<bool> seen = stackalloc bool[256];
+            seen[s[0]] = true;
+
+            var left = 0;
+            var max = 1;
+
+            for(var i = 1; i < s.Length; i++)
+            {
+                var c = s[i];
+                if(!seen[c])
+                {
+                    seen[c] = true;
+                    max = Math.Max(max, i - left + 1);
+                    continue;
+                }
+
+                // We need need to move left to the first character after c from the left
+                left++;
+                while(s[left - 1] != c)
+                {
+                    seen[s[left - 1]] = false;
+                    left++;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q3_LongestSubstringWithoutRepeatingCharacters/SolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q3_LongestSubstringWithoutRepeatingCharacters;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q3_LongestSubstringWithoutRepeatingCharacters
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        [TestCase("a", 1)]
+        [TestCase("", 0)]
+        [TestCase("abcdefg", 7)]
+        [TestCase("tmmzuxt", 5)]
+        public void Examples(string value, int expected)
+        {
+            var s = new Solution();
+            Assert.That(s.LengthOfLongestSubstring(value), Is.EqualTo(expected));
+        }
+    }
+}
