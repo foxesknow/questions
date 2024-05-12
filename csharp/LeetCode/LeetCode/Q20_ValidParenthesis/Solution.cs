40e27324bd8432ad8401ca369f3dbbb2296df19f
foxesknow <seanbatten58@gmail.com>
2024-05-12 17:15:05 +0100

    Question 20

---
+++ csharp/LeetCode/LeetCode/Q20_ValidParenthesis/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q20_ValidParenthesis
+{
+    public class Solution
+    {
+        public bool IsValid(string s) 
+        {
+            var stack = new Stack<char>();
+
+            foreach(var c in s)
+            {
+                if(c is ('(' or '{' or '['))
+                {
+                    stack.Push(c);
+                    continue;
+                }
+
+                // It's a close token
+                if(stack.Count == 0) return false;
+
+                var isValid = stack.Pop() switch
+                {
+                    '(' when c == ')' => true,
+                    '{' when c == '}' => true,
+                    '[' when c == ']' => true,
+                    _ => false
+                };
+
+                if(!isValid) return false;
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q20_ValidParenthesis/SolutionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q20_ValidParenthesis;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q20_ValidParenthesis
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("()", true)]
+        [TestCase("()[]{}", true)]
+        [TestCase("(}", false)]
+        [TestCase("(", false)]
+        [TestCase("([{}])", true)]
+        public void Example(string expression, bool expected)
+        {
+            var s = new Solution();
+            Assert.That(s.IsValid(expression), Is.EqualTo(expected));
+        }
+    }
+}
