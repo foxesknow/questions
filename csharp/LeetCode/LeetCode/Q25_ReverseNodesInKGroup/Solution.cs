b384a1f51066d9df5c41a65782a5fd28f3a5f48f
foxesknow <seanbatten58@gmail.com>
2024-05-13 17:32:14 +0100

    Question 25

---
+++ csharp/LeetCode/LeetCode/Q25_ReverseNodesInKGroup/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q25_ReverseNodesInKGroup
+{
+    public class Solution
+    {
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            var (reversedHead, tail, node) = ReverseGroup(head, k);
+
+            while(node is not null)
+            {
+                var (h, t, n) = ReverseGroup(node, k);
+
+                tail.next = h;
+                tail = t;
+                node = n;
+            }
+
+            return reversedHead;
+        }
+
+        private bool CanConsume(ListNode list, int count)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                if(list is null) return false;
+
+                list = list.next;
+            }
+
+            return true;
+        }
+
+        private (ListNode GroupHead, ListNode GroupTail, ListNode Tail) ReverseGroup(ListNode node, int k)
+        {
+            if(!CanConsume(node, k)) return (node, null, null);
+
+            ListNode head = null;
+            ListNode tail = null;
+
+            for(int i = 0; i < k && node is not null; i++)
+            {
+                var next = node.next;
+
+                if(head is null)
+                {
+                    head = tail = node;
+                    node.next = null;
+                }
+                else
+                {
+                    node.next = head;
+                    head = node;
+                }
+
+                node = next;
+            }
+
+            return (head, tail, node);
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q25_ReverseNodesInKGroup/SolutionTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using LeetCode.Q25_ReverseNodesInKGroup;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q25_ReverseNodesInKGroup
+{
+    [TestFixture]
+    public class SolutionTest : SolutionTestBase
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3, 4, 5);
+            var reversed = s.ReverseKGroup(list, 2);
+            EnsureListEquals(reversed, 2, 1, 4, 3, 5);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3, 4, 5);
+            var reversed = s.ReverseKGroup(list, 3);
+            EnsureListEquals(reversed, 3, 2, 1, 4, 5);
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3, 4, 5);
+            var reversed = s.ReverseKGroup(list, 5);
+            EnsureListEquals(reversed, 5, 4, 3, 2, 1);
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3, 4, 5);
+            var reversed = s.ReverseKGroup(list, 6);
+            EnsureListEquals(reversed, 1, 2, 3, 4, 5);
+        }
+    }
+}
