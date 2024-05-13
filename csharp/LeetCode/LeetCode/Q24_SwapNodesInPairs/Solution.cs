68803a1f31521ce8bc886d528f8e522cfcc116aa
foxesknow <seanbatten58@gmail.com>
2024-05-13 17:54:57 +0100

    Question 24

---
+++ csharp/LeetCode/LeetCode/Q24_SwapNodesInPairs/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q24_SwapNodesInPairs
+{
+    public class Solution
+    {
+        public ListNode SwapPairs(ListNode head) 
+        {
+            if(head is null || head.next is null) return head;
+
+            ListNode swapHead = null;
+            ListNode tail = null;
+
+            while(head is not null)
+            {
+                if(head.next is null)
+                {
+                    if(swapHead is null)
+                    {
+                        swapHead = head;
+                        
+                    }
+                    else
+                    {
+                        tail.next = head;
+                    }
+
+                    head = head.next;
+                }
+                else
+                {
+                    // There's at least 2 items in from
+                    var newHead = head.next.next;
+
+                    var swap = head.next;
+                    swap.next = head;
+                    swap.next.next = null;
+
+                    if(swapHead is null)
+                    {
+                        swapHead = swap;                        
+                    }
+                    else
+                    {
+                        tail.next = swap;
+                    }
+
+                    tail = swap.next;
+                    head = newHead;
+                }
+            }
+
+            return swapHead;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q24_SwapNodesInPairs/SolutionTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using LeetCode.Q24_SwapNodesInPairs;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q24_SwapNodesInPairs
+{
+    [TestFixture]
+    public class SolutionTest : SolutionTestBase
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3, 4);
+            var swapped = s.SwapPairs(list);
+            EnsureListEquals(swapped, 2, 1, 4, 3);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3, 4);
+            var swapped = s.SwapPairs(null);
+            Assert.That(swapped, Is.Null);
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1);
+            var swapped = s.SwapPairs(list);
+            EnsureListEquals(swapped, 1);
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3);
+            var swapped = s.SwapPairs(list);
+            EnsureListEquals(swapped, 2, 1, 3);
+        }
+
+        [Test]
+        public void Example5()
+        {
+            var s = new Solution();
+            var list = ListNode.Make(1, 2, 3, 4, 5);
+            var swapped = s.SwapPairs(list);
+            EnsureListEquals(swapped, 2, 1, 4, 3, 5);
+        }
+    }
+}
