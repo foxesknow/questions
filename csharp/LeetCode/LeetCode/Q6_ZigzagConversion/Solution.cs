2c869b7041f26b37b3d8f870fcaa190790445ffd
foxesknow <seanbatten58@gmail.com>
2024-05-15 09:49:21 +0100

    Question 6

---
+++ csharp/LeetCode/LeetCode/Q6_ZigzagConversion/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q6_ZigzagConversion
+{
+    public class Solution
+    {
+        public string Convert(string s, int numRows) 
+        {
+            if(numRows == 1) return s;
+
+            var sequence = Zigzag(s, numRows).OrderBy(x => x.Row).ThenBy(x => x.Col);
+            return new string(sequence.Select(x => x.C).ToArray());
+        }
+
+        private IEnumerable<(int Row, int Col, char C)> Zigzag(string s, int numRows)
+        {
+            var row = 0;
+            var rowAdjust = 1;
+            var col = 0;
+            var colAdjust = 0;
+
+            foreach(char c in s)
+            {
+                yield return (row, col, c);
+                
+                row += rowAdjust;
+                col += colAdjust;
+
+                if(row == numRows)
+                {
+                    row -= 2;
+                    rowAdjust = -1;
+
+                    col++;
+                    colAdjust = 1;
+                }
+                else if(row == -1)
+                {
+                    row = 1;
+                    rowAdjust = 1;
+
+                    col--;
+                    colAdjust = 0;
+                }
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q6_ZigzagConversion/SolutionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q6_ZigzagConversion;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q6_ZigzagConversion
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        [TestCase("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        [TestCase("A", 1, "A")]
+        [TestCase("AB", 1, "AB")]
+        public void Examples(string input, int numRows, string expected)
+        {
+            var s = new Solution();
+            var zig = s.Convert(input, numRows);
+            Assert.That(zig, Is.EqualTo(expected));
+        }
+    }
+}
