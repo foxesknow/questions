28462f284ac43d0fcdeb517379dd9fd8d2907b18
foxesknow <seanbatten58@gmail.com>
2024-05-15 17:59:19 +0100

    Question 34

---
+++ csharp/LeetCode/LeetCode/Q34_FindFirstAndLast/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q34_FindFirstAndLast
+{
+    public class Solution
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var index1 = LowerBound(nums, target);
+
+            if(index1 == -1) return new int[]{-1, -1};
+
+            var index2 = UpperBound(nums, target);
+
+            return new[]{index1, index2};
+        }
+
+        private int LowerBound(int[] nums, int target)
+        {
+            return BoundedSearch(nums, target, SearchType.Lower);
+        }
+
+        private int UpperBound(int[] nums, int target)
+        {
+            return BoundedSearch(nums, target, SearchType.Upper);
+        }
+
+        private int BoundedSearch(int[] nums, int target, SearchType searchType)
+        {
+            var index = -1;
+            int left = 0, right = nums.Length - 1;
+
+            while(left <= right)
+            {
+                var mid = (left + right) / 2;
+                var value = nums[mid];
+
+                if(value == target)
+                {
+                    index = mid;
+
+                    if(searchType == SearchType.Lower)
+                    {
+                        right = mid - 1;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else if(value < target)
+                {
+                    left = mid + 1;                    
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return index;
+        }
+
+        enum SearchType
+        {
+            Upper,
+            Lower
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q34_FindFirstAndLast/SolutionTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q34_FindFirstAndLast;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q34_FindFirstAndLast
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var nums = new int[]{5,7,7,8,8,10};
+            var indexes = s.SearchRange(nums, 8);
+            Assert.That(indexes[0], Is.EqualTo(3));
+            Assert.That(indexes[1], Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var nums = new int[]{5,7,7,8,8,10};
+            var indexes = s.SearchRange(nums, 6);
+            Assert.That(indexes[0], Is.EqualTo(-1));
+            Assert.That(indexes[1], Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var nums = new int[]{};
+            var indexes = s.SearchRange(nums, 0);
+            Assert.That(indexes[0], Is.EqualTo(-1));
+            Assert.That(indexes[1], Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            var nums = new int[]{5,7,7,8,8,10};
+            var indexes = s.SearchRange(nums, 10);
+            Assert.That(indexes[0], Is.EqualTo(5));
+            Assert.That(indexes[1], Is.EqualTo(5));
+        }
+    }
+}
