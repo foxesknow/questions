c8e94fbdb0f42d24888cfa537f2c0dcfaeebf161
foxesknow <seanbatten58@gmail.com>
2024-05-17 15:05:26 +0100

    Question 69

---
+++ csharp/LeetCode/LeetCode/Q69_Sqrt/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q69_Sqrt
+{
+    public class Solution
+    {
+        public int MySqrt(int x) 
+        {
+            if(x == 0 || x == 1) return x;
+
+            long? last = null;
+            long lower = 1, upper = x;
+
+            while(true)
+            {
+                long mid = (lower + upper) / 2;
+                if(last is not null && last == mid) return (int)mid;
+
+                var sqr = mid * mid;
+                if(sqr == x)
+                {
+                    return (int)mid;
+                }
+                else if(sqr > x)
+                {
+                    upper = mid;
+                }
+                else
+                {
+                    lower = mid;
+                }
+
+                last = mid;
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q69_Sqrt/SolutionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q69_Sqrt;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q69_Sqrt
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase(4, 2)]
+        [TestCase(16, 4)]
+        [TestCase(121, 11)]
+        [TestCase(196, 14)]
+        [TestCase(8, 2)]
+        [TestCase(85, 9)]
+        [TestCase(2147395599, 46339)]
+        public void Examples(int value, int expected)
+        {
+            var s = new Solution();
+            Assert.That(s.MySqrt(value), Is.EqualTo(expected));
+        }
+    }
+}
