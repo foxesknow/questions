bc64105bafb77cbd9be64d528561e3ecc5593b5a
foxesknow <seanbatten58@gmail.com>
2024-05-17 13:44:34 +0100

    Question 48

---
+++ csharp/LeetCode/LeetCode/Q48_RotateImage/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q48_RotateImage
+{
+    public class Solution
+    {
+        public void Rotate(int[][] matrix) 
+        {
+            var n = matrix[0].Length;
+            
+            // First, flip it across the diagonal
+            for(var row = 0; row < n; row++)
+            {
+                for(var col = 0; col < row; col++)
+                {
+                    (matrix[row][col], matrix[col][row]) = (matrix[col][row], matrix[row][col]);
+                }
+            }
+
+            // Now reverse each row
+            foreach(var row in matrix)
+            {
+                Array.Reverse(row);
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q48_RotateImage/SolutionTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q48_RotateImage;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q48_RotateImage
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            int[][] matrix = new int[][]
+            {
+                new int[]{1, 2, 3},
+                new int[]{4, 5, 6},
+                new int[]{7, 8, 9},
+            };
+
+            s.Rotate(matrix);
+
+            Assert.That(matrix[0], Is.EqualTo(new int[]{7, 4, 1}));
+            Assert.That(matrix[1], Is.EqualTo(new int[]{8, 5, 2}));
+            Assert.That(matrix[2], Is.EqualTo(new int[]{9, 6, 3}));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            int[][] matrix = new int[][]
+            {
+                new int[]{1}
+            };
+
+            s.Rotate(matrix);
+
+            Assert.That(matrix[0], Is.EqualTo(new int[]{1}));
+        }
+    }
+}
