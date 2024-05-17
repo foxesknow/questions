294ac9eba70b731df1e83e213b23b453224530a9
foxesknow <seanbatten58@gmail.com>
2024-05-17 14:05:26 +0100

    Question 50

---
+++ csharp/LeetCode/LeetCode/Q50_Pow/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q50_Pow
+{
+    public class Solution
+    {
+        public double MyPow(double x, int n) 
+        {
+            if(x == 1 || n == 0) return 1;
+            if(n == 1) return x;
+            if(n == -1) return 1 / x;
+            if(x == 0) return 0;
+
+            var half = MyPow(x, n / 2);
+            var value = half * half;
+
+            if((n & 1) == 1)
+            {
+                // We've got an odd power, so we need to scale the number up one more.
+                // Also, because the power is odd sign of the power is relevant
+                var sign = (n >= 0 ? 1 : -1);
+                return value * MyPow(x, sign);
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q50_Pow/SolutionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q50_Pow;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q50_Pow
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase(2d, 10, 1024d)]
+        [TestCase(2.1d, 3, 9.2610d)]
+        [TestCase(2d, -2, 0.25d)]
+        [TestCase(10d, 0, 1d)]
+        [TestCase(10d, 1, 10d)]
+        public void Examples(double x, int n, double expected)
+        {
+            var s = new Solution();
+            var result = s.MyPow(x, n);
+            Assert.That(result, Is.EqualTo(expected).Within(0.00000001d));
+        }
+    }
+}
