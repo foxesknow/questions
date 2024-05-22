b10c7c1e20ed9e93cf36802ad06c54cf592dd579
foxesknow <seanbatten58@gmail.com>
2024-05-22 12:29:28 +0100

    Questin 3105

---
+++ csharp/LeetCode/LeetCode/Q3105_LongestIncreasingOrDecreasing/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q3105_LongestIncreasingOrDecreasing
+{
+    public class Solution
+    {
+        public int LongestMonotonicSubarray(int[] nums) 
+        {
+            var increasing = LongestIncreasingRun(nums);
+            Array.Reverse(nums);
+            var decreasing = LongestIncreasingRun(nums);
+
+            return Math.Max(increasing, decreasing);
+        }
+
+        private int LongestIncreasingRun(int[] nums)
+        {
+            var longest = 1;
+
+            var left = 0;
+
+            for(var i = 1; i < nums.Length; i++)
+            {
+                if(nums[i] > nums[i - 1])
+                {
+                    longest = Math.Max(longest, i - left + 1);
+                }
+                else
+                {
+                    left = i;
+                }
+            }
+
+            return longest;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q3105_LongestIncreasingOrDecreasing/SolutionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q3105_LongestIncreasingOrDecreasing;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q3105_LongestIncreasingOrDecreasing
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var nums = new int[]{1, 4, 3, 3, 2};
+            Assert.That(s.LongestMonotonicSubarray(nums), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var nums = new int[]{3, 3, 3, 3};
+            Assert.That(s.LongestMonotonicSubarray(nums), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var nums = new int[]{3, 2, 1};
+            Assert.That(s.LongestMonotonicSubarray(nums), Is.EqualTo(3));
+        }
+    }
+}
