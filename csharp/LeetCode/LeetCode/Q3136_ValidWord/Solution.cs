1cc4ea78ea4a0ce3b86668b0a84282df7047abab
foxesknow <seanbatten58@gmail.com>
2024-05-22 10:39:32 +0100

    Question 3136

---
+++ csharp/LeetCode/LeetCode/Q3136_ValidWord/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q3136_ValidWord
+{
+    public class Solution
+    {
+        public bool IsValid(string word) 
+        {
+            if(word.Length < 3) return false;
+
+            var vowelCount = 0;
+            var consonantCount = 0;
+            
+            foreach(var c in word)
+            {
+                if(char.IsDigit(c)) continue;
+
+                if(char.IsLetter(c) == false) return false;
+
+                if(IsVowel(c))
+                {
+                    vowelCount++;
+                }
+                else if(IsConsonant(c))
+                {
+                    consonantCount++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return vowelCount > 0 && consonantCount > 0;
+        }
+
+        private bool IsVowel(char c)
+        {
+            return char.ToLower(c) is ('a' or 'e' or 'i' or 'o' or 'u'); 
+        }
+
+        private bool IsConsonant(char c)
+        {
+            return !IsVowel(c);
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q3136_ValidWord/SolutionTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q3136_ValidWord;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q3136_ValidWord
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        [TestCase("234Adas", true)]
+        [TestCase("b3", false)]
+        [TestCase("a3$e", false)]
+        public void Examples(string word, bool expected)
+        {
+            var s = new Solution();
+            Assert.That(s.IsValid(word), Is.EqualTo(expected));
+        }
+    }
+}
