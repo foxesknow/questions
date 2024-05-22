58e43faaad386986360b76d5281e6b5d3db1b76e
foxesknow <seanbatten58@gmail.com>
2024-05-22 10:15:43 +0100

    Question 3151

---
+++ csharp/LeetCode/LeetCode/Q3151_SpecialArray1/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q3151_SpecialArray1
+{
+    public class Solution
+    {
+        public bool IsArraySpecial(int[] nums) 
+        {
+            if(nums.Length == 1) return true;
+
+            for(var i = 1; i < nums.Length; i++)
+            {
+                var rhs = Parity(nums[i]);
+                var lhs = Parity(nums[i - 1]);
+
+                if(lhs ^ rhs) continue;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool Parity(int number)
+        {
+            return (number & 1) == 1;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q3151_SpecialArray1/SolutionTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q3151_SpecialArray1;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q3151_SpecialArray1
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var nums = new int[]{1};
+            Assert.That(s.IsArraySpecial(nums), Is.True);
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var nums = new int[]{2, 1, 4};
+            Assert.That(s.IsArraySpecial(nums), Is.True);
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var nums = new int[]{4, 3, 1, 6};
+            Assert.That(s.IsArraySpecial(nums), Is.False);
+        }
+    }
+}
