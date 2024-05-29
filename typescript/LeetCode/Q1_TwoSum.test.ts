99ada07806f0e5aaf56f6db6359aa84e6f1956f8
Sean <seanbatten58@gmail.com>
2024-05-29 16:48:35 +0100

    Typescript questions

---
+++ typescript/LeetCode/Q1_TwoSum.test.ts
@@ -0,0 +1,34 @@
+function twoSum(nums: number[], target: number): number[] {
+    let cache = new Map<number, number>();
+
+    for (var i = 0; i < nums.length; i++) {
+        let diff = target - nums[i];
+        
+        let index = cache.get(diff);
+        if(index != undefined) {
+            return [i, index];
+        }
+
+        cache.set(nums[i], i);
+    }
+
+    return null;
+}
+
+test("example 1", () => {
+    let result = twoSum([2,7,11,15], 9);
+    expect(result).toContain(0);
+    expect(result).toContain(1);
+});
+
+test("example 2", () => {
+    let result = twoSum([3,2,4], 6);
+    expect(result).toContain(1);
+    expect(result).toContain(2);
+});
+
+test("example 3", () => {
+    let result = twoSum([3,3], 6);
+    expect(result).toContain(0);
+    expect(result).toContain(0);
+});
