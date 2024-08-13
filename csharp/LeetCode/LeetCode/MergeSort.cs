4ca8f482c14c8f2c9264223b541e0e5e05b1b570
foxesknow <seanbatten58@gmail.com>
2024-08-13 19:41:28 +0100

    Merge sort

---
+++ csharp/LeetCode/LeetCode/MergeSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MergeSort
+    {
+        private void Combine(int[] numbers, int left, int mid, int right)
+        {
+            var leftLength = mid - left + 1;
+            var rightLength = right - mid;
+
+            // We need to create local copies of the data as we're going to 
+            // be overwriting the incoming array
+            var leftScratch = new int[leftLength];
+            Array.Copy(numbers, left, leftScratch, 0, leftScratch.Length);
+
+            var rightScratch = new int[rightLength];
+            Array.Copy(numbers, mid + 1, rightScratch, 0, rightScratch.Length);
+
+            var offset = left;
+            var l = 0;
+            var r = 0;
+
+            // Combine the 2 lists until we reach the end of one or both
+            while(l < leftLength && r < rightLength)
+            {
+                if(leftScratch[l] < rightScratch[r])
+                {
+                    numbers[offset++] = leftScratch[l++];
+                }
+                else
+                {
+                    numbers[offset++] = rightScratch[r++];
+                }
+            }
+
+            if(l < leftLength)
+            {
+                var itemsToCopy = leftLength - l;
+                Array.Copy(leftScratch, l, numbers, offset, itemsToCopy);
+                offset += itemsToCopy;
+            }
+
+            if(r < rightLength)
+            {
+                var itemsToCopy = rightLength - r;
+                Array.Copy(rightScratch, r, numbers, offset, itemsToCopy);
+            }
+        }
+
+        private void Sort(int[] numbers, int left, int right)
+        {
+            if(left < right)
+            {
+                var mid = left + (right - left) / 2;
+                Sort(numbers, left, mid);
+                Sort(numbers, mid + 1, right);
+                Combine(numbers, left, mid, right);
+            }
+        }
+
+        public void Sort(int[] numbers)
+        {
+            Sort(numbers, 0, numbers.Length - 1);
+            //mergeSort(numbers, 0, numbers.Length - 1);
+        }
+    }
+}
+
---
+++ csharp/LeetCode/LeetCodeTests/MergeSortTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class MergeSortTests
+    {
+        [Test]
+        [TestCase(new int[]{1})]
+        [TestCase(new int[]{1, 2})]
+        [TestCase(new int[]{2, 1})]
+        [TestCase(new int[]{5, 4, 3, 2, 1})]
+        [TestCase(new int[]{12, 11, 13, 5, 6, 7})]
+        public void Sort(int[] numbers)
+        {
+            var expected = new int[numbers.Length];
+            Array.Copy(numbers, expected, numbers.Length);
+            Array.Sort(expected);
+            
+            var mergeSort = new MergeSort();
+            mergeSort.Sort(numbers);
+
+            Assert.That(numbers, Is.EqualTo(expected));
+
+        }
+    }
+}
