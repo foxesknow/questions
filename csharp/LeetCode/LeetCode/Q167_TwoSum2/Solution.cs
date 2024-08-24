d102651ada5499d0b1e702a35ac2b20474142516
foxesknow <seanbatten58@gmail.com>
2024-08-24 11:15:55 +0100

    Question 167

---
+++ csharp/LeetCode/LeetCode/Q167_TwoSum2/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q167_TwoSum2
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target) 
+        {
+            var left = 0;
+            var right = numbers.Length - 1;
+
+            while(left < right)
+            {
+                var sum = numbers[left] + numbers[right];
+                if(sum == target)
+                {
+                    // +1 as they want the indexes to be 1 bases for some reason!
+                    return new[]{left + 1, right + 1};
+                }
+
+                if(sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+
+            throw new Exception("we shouldn't get here!");
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q167_TwoSum2/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q167_TwoSum2;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q167_TwoSum2
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        [TestCase(new int[]{2, 7, 11, 15}, 9, new int[]{1, 2})]
+        [TestCase(new int[]{2, 3, 4}, 6, new int[]{1, 3})]
+        [TestCase(new int[]{-1, 0}, -1, new int[]{1, 2})]
+        public void TwoSum(int[] values, int sum, int[] expected)
+        {
+            var s = new Solution();
+            var results = s.TwoSum(values, sum);
+            Assert.That(results[0], Is.EqualTo(expected[0]));
+            Assert.That(results[1], Is.EqualTo(expected[1]));
+        }
+    }
+}
