b81c0e6d23b23cebde4334dbb61fbe3d9175a3c1
foxesknow <seanbatten58@gmail.com>
2024-08-24 12:03:11 +0100

    Question 38

---
+++ csharp/LeetCode/LeetCode/Q38_CountAndSay/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q38_CountAndSay
+{
+    public class Solution
+    {
+        public string CountAndSay(int n) 
+        {
+            var value = "1";
+
+            for(int i = 2; i <= n; i++)
+            {
+                value = Expand(value);
+            }
+
+            return value;
+        }
+
+        private string Expand(string input)
+        {
+            var b = new StringBuilder();
+
+            var count = 1;
+            var current = input[0];
+
+            for(int i = 1; i < input.Length; i++)
+            {
+                var c = input[i];
+
+                if(c != current)
+                {
+                    b.Append(count);
+                    b.Append(current);
+                    current = c;
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+
+            b.Append(count);
+            b.Append(current);
+
+            return b.ToString();
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q38_CountAndSay/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q38_CountAndSay;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q38_CountAndSay
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        [TestCase(4, "1211")]
+        [TestCase(5, "111221")]
+        public void CountAndSay(int count, string expected)
+        {
+            var s = new Solution();
+            Assert.That(s.CountAndSay(count), Is.EqualTo(expected));
+        }
+    }
+}
