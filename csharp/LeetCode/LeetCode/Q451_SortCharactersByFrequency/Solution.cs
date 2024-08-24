63f00c4222dd0688d3f98d4d3ee421b857acf4a4
foxesknow <seanbatten58@gmail.com>
2024-08-24 10:52:02 +0100

    Question 451

---
+++ csharp/LeetCode/LeetCode/Q451_SortCharactersByFrequency/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q451_SortCharactersByFrequency
+{
+    public class Solution
+    {
+        public string FrequencySort(string s) 
+        {
+            var frequencies = new Dictionary<char, int>();
+
+            foreach(var c in s)
+            {
+                if(frequencies.TryGetValue(c, out var count))
+                {
+                    frequencies[c] = count + 1;
+                }
+                else
+                {
+                    frequencies.Add(c, 1);
+                }
+            }
+
+            var counts = frequencies.ToList();
+            counts.Sort((left, right) => right.Value.CompareTo(left.Value));
+
+            var sum = counts.Sum(pair => pair.Value);
+            var characters = new char[sum];
+            var index = 0;
+
+            foreach(var pair in counts)
+            {
+                char c = pair.Key;
+
+                for(int i = 0; i < pair.Value; i++)
+                {
+                    characters[index++] = c;
+                }
+            }
+            
+            return new string(characters);
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q451_SortCharactersByFrequency/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q451_SortCharactersByFrequency;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q451_SortCharactersByFrequency
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        [TestCase("tree", "eert")]
+        public void FrequencySort(string value, string expected)
+        {
+            var s = new Solution();
+            Assert.That(s.FrequencySort(value), Is.EqualTo(expected));
+        }
+    }
+}
