f8f7a3ec34d9ae1a19cf8e8aeace8331822ae8d3
foxesknow <seanbatten58@gmail.com>
2024-08-26 16:21:53 +0100

    Question 1114

---
+++ csharp/LeetCode/LeetCode/Q1114_PrintInOrder/Foo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q1114_PrintInOrder
+{
+    internal class Foo
+    {
+        private readonly object m_SyncRoot = new();
+        private int m_NextState = 1;
+
+        public Foo() 
+        {
+        
+        }
+
+        public void First(Action printFirst) 
+        {
+            lock(m_SyncRoot)
+            {
+                // printFirst() outputs "first". Do not change or remove this line.
+                printFirst();
+
+                m_NextState = 2;
+                System.Threading.Monitor.PulseAll(m_SyncRoot);
+            }
+        }
+
+        public void Second(Action printSecond) 
+        {
+            lock(m_SyncRoot)
+            {
+                while(m_NextState != 2)
+                {
+                    System.Threading.Monitor.Wait(m_SyncRoot);
+                }
+
+                // printSecond() outputs "second". Do not change or remove this line.
+                printSecond();
+                m_NextState = 3;
+
+                System.Threading.Monitor.PulseAll(m_SyncRoot);
+            }
+        }
+
+        public void Third(Action printThird) 
+        {
+            lock(m_SyncRoot)
+            {
+                while(m_NextState != 3)
+                {
+                    System.Threading.Monitor.Wait(m_SyncRoot);
+                }
+
+                // printThird() outputs "third". Do not change or remove this line.
+                printThird();
+            }
+        }
+    }
+}
