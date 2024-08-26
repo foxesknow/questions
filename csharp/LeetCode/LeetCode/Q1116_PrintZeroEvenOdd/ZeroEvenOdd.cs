bef0c19b44ae53dac0fe4e89e15d986b7a47de4a
foxesknow <seanbatten58@gmail.com>
2024-08-26 17:16:12 +0100

    Question 1116

---
+++ csharp/LeetCode/LeetCode/Q1116_PrintZeroEvenOdd/ZeroEvenOdd.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q1116_PrintZeroEvenOdd
+{
+    public class ZeroEvenOdd
+    {
+        private int n;
+
+        private readonly object m_SyncRoot = new();
+        private bool m_DoZero = true;
+        private int m_Next = 1;
+    
+        public ZeroEvenOdd(int n) 
+        {
+            this.n = n;
+        }
+
+        // printNumber(x) outputs "x", where x is an integer.
+        public void Zero(Action<int> printNumber) 
+        {
+            while(true)
+            {
+                lock(m_SyncRoot)
+                {
+                    while(!m_DoZero && m_Next <= n)
+                    {
+                        System.Threading.Monitor.Wait(m_SyncRoot);
+                    }
+
+                    if(m_Next > n) return;
+
+                    printNumber(0);
+                    m_DoZero = false;
+
+                    System.Threading.Monitor.PulseAll(m_SyncRoot);
+                }
+            }
+        }
+
+        public void Even(Action<int> printNumber) 
+        {
+            while(true)
+            {
+                lock(m_SyncRoot)
+                {
+                    while((m_DoZero || (m_Next & 1) != 0) && m_Next <= n)
+                    {
+                        System.Threading.Monitor.Wait(m_SyncRoot);
+                    }
+
+                    if(m_Next > n) return;
+
+                    printNumber(m_Next);
+                    m_Next++;
+
+                    m_DoZero = true;
+
+                    System.Threading.Monitor.PulseAll(m_SyncRoot);
+                }
+            }
+        }
+
+        public void Odd(Action<int> printNumber) 
+        {
+            while(true)
+            {
+                lock(m_SyncRoot)
+                {
+                    while((m_DoZero || (m_Next & 1) != 1) && m_Next <= n)
+                    {
+                        System.Threading.Monitor.Wait(m_SyncRoot);
+                    }
+
+                    if(m_Next > n) return;
+
+                    printNumber(m_Next);
+                    m_Next++;
+
+                    m_DoZero = true;
+
+                    System.Threading.Monitor.PulseAll(m_SyncRoot);
+                }
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q1116_PrintZeroEvenOdd/ZeroEvenOddTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using LeetCode.Q1116_PrintZeroEvenOdd;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q1116_PrintZeroEvenOdd
+{
+    [TestFixture]
+    public class ZeroEvenOddTests
+    {
+        [Test]
+        public void Execute()
+        {
+            var s = new ZeroEvenOdd(2);
+
+            var b = new StringBuilder();
+            Action<int> printNumber = n =>
+            {
+                lock(b)
+                {
+                    b.Append(n);
+                }
+            };
+
+            var oddThread = new Thread(() => s.Odd(printNumber));
+            oddThread.Start();
+
+            var evenThread = new Thread(() => s.Even(printNumber));
+            evenThread.Start();
+
+            var zeroThread = new Thread(() => s.Zero(printNumber));
+            zeroThread.Start();
+
+            oddThread.Join();
+            evenThread.Join();
+            zeroThread.Join();
+
+            Assert.That(b.ToString(), Is.EqualTo("0102"));
+        }
+    }
+}
