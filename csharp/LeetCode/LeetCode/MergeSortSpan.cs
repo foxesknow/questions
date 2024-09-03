364403aa809983de2fc6f734bb2dc1107ff929aa
foxesknow <seanbatten58@gmail.com>
2024-09-03 21:18:34 +0100

    Merge sort with spans

---
+++ csharp/LeetCode/LeetCode/MergeSortSpan.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MergeSortSpan
+    {
+        private void Combine(Span<int> destination, Span<int> left, Span<int> right)
+        {
+            Span<int> buffer = new int[left.Length + right.Length];
+            var offset = 0;
+
+            while(!left.IsEmpty && !right.IsEmpty)
+            {
+                if(left[0] <= right[0])
+                {
+                    buffer[offset++] = left[0];
+                    left = left.Slice(1);
+                }
+                else
+                {
+                    buffer[offset++] = right[0];
+                    right = right.Slice(1);
+                }
+            }
+
+            if(left.Length != 0)
+            {
+                left.CopyTo(buffer.Slice(offset));
+            }
+            else
+            {
+                right.CopyTo(buffer.Slice(offset));
+            }
+
+            buffer.CopyTo(destination);
+        }
+
+        public void Sort(Span<int> numbers)
+        {
+            if(numbers.Length > 1)
+            {
+                var mid = numbers.Length / 2;
+                
+                var left = numbers[0..mid];
+                var right = numbers.Slice(mid);
+
+                Sort(left);
+                Sort(right);
+
+                Combine(numbers, left, right);
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/MergeSortSpanTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class MergeSortSpanTests
+    {
+        [Test]
+        [TestCase(new int[]{1})]
+        [TestCase(new int[]{1, 2})]
+        [TestCase(new int[]{2, 1})]
+        [TestCase(new int[]{2, 1,3 })]
+        [TestCase(new int[]{5, 4, 3, 2, 1})]
+        [TestCase(new int[]{12, 11, 13, 5, 6, 7})]
+        public void Sort(int[] numbers)
+        {
+            var expected = new int[numbers.Length];
+            Array.Copy(numbers, expected, numbers.Length);
+            Array.Sort(expected);
+            
+            var mergeSort = new MergeSortSpan();
+            mergeSort.Sort(numbers);
+
+            Assert.That(numbers, Is.EqualTo(expected));
+        }
+    }
+}
