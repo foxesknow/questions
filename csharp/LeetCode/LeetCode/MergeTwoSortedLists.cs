82aadf2745f00136d3a2c488607cb239dea9a557
foxesknow <seanbatten58@gmail.com>
2024-09-12 14:00:52 +0100

    Added MergeTwoSortedLists

---
+++ csharp/LeetCode/LeetCode/MergeTwoSortedLists.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MergeTwoSortedLists
+    {
+        public IReadOnlyList<int> Merge(IReadOnlyList<int> left, IReadOnlyList<int> right)
+        {
+            var destination = new int[left.Count + right.Count];
+
+            var leftOffset = 0;
+            var rightOffset = 0;
+            var destinationOffset = 0;
+
+            while(leftOffset < left.Count && rightOffset < right.Count)
+            {
+                if(left[leftOffset] <= right[rightOffset])
+                {
+                    destination[destinationOffset++] = left[leftOffset++];
+                }
+                else
+                {
+                    destination[destinationOffset++] = right[rightOffset++];
+                }
+            }
+
+            while(leftOffset < left.Count)
+            {
+                destination[destinationOffset++] = left[leftOffset++];
+            }
+
+            while(rightOffset < right.Count)
+            {
+                destination[destinationOffset++] = right[rightOffset++];
+            }
+
+            return destination;
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/MergeTwoSortedListsTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    [TestFixture]
+    public class MergeTwoSortedListsTests
+    {
+        [Test]
+        [TestCase(new int[]{1, 2, 3}, new int[]{})]
+        [TestCase(new int[]{}, new int[]{1, 2, 3})]
+        [TestCase(new int[]{1, 2, 3}, new int[]{1, 2, 3})]
+        [TestCase(new int[]{1, 2, 3}, new int[]{4, 5, 6})]
+        [TestCase(new int[]{1, 2, 3, 15}, new int[]{4, 5, 6, 7, 8, 9})]
+        public void Merge(int[] left, int[] right)
+        {
+            var expected = new List<int>();
+            expected.AddRange(left);
+            expected.AddRange(right);
+            expected.Sort();
+
+            var merger = new MergeTwoSortedLists();
+            var result = merger.Merge(left, right);
+
+            Assert.That(result, Is.EqualTo(expected.ToArray()));
+        }
+    }
+}
