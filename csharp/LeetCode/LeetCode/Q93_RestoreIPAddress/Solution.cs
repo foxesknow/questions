bcbc6c682be64fc838196688c79d0702465b2be6
foxesknow <seanbatten58@gmail.com>
2024-11-25 11:41:55 +0000

    Question 93

---
+++ csharp/LeetCode/LeetCode/Q93_RestoreIPAddress/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q93_RestoreIPAddress
+{
+    public class Solution 
+    {
+        public IList<string> RestoreIpAddresses(string s) 
+        {
+            var numbers = s.AsSpan();
+            Span<int> acc = stackalloc int[4];
+
+            var results = new List<string>();
+            Recurse(0, numbers, acc, results);
+
+            return results;
+        }
+
+        private void Recurse(int level, ReadOnlySpan<char> numbers, Span<int> acc, List<string> results)
+        {
+            if(level == 4)
+            {
+                if(numbers.IsEmpty)
+                {
+                    var ipAddress = string.Join('.', acc.ToArray());
+                    results.Add(ipAddress);
+                }
+                
+                return;
+            }
+
+            if(numbers.IsEmpty) return;
+
+            var integer = numbers[0] - '0';
+            while(integer < 256)
+            {
+                acc[level] = integer;
+                numbers = numbers[1..];
+                
+                Recurse(level + 1, numbers, acc, results);
+
+                if(numbers.IsEmpty) break;
+                
+                // Zero is only allowed on its own, never as a prefix,
+                // so we should stop after trying it
+                if(integer == 0) break;
+
+                integer = (integer * 10) + (numbers[0] - '0'); 
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q93_RestoreIPAddress/SolutionTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode;
+using LeetCode.Q93_RestoreIPAddress;
+
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q93_RestoreIPAddress
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            var results = s.RestoreIpAddresses("25525511135");
+            Assert.That(results, Has.Count.EqualTo(2));
+            Assert.That(results, Contains.Item("255.255.11.135"));
+            Assert.That(results, Contains.Item("255.255.111.35"));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            var results = s.RestoreIpAddresses("0000");
+            Assert.That(results, Has.Count.EqualTo(1));
+            Assert.That(results, Contains.Item("0.0.0.0"));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            var results = s.RestoreIpAddresses("101023");
+            Assert.That(results, Has.Count.EqualTo(5));
+            Assert.That(results, Contains.Item("1.0.10.23"));
+            Assert.That(results, Contains.Item("1.0.102.3"));
+            Assert.That(results, Contains.Item("10.1.0.23"));
+            Assert.That(results, Contains.Item("10.10.2.3"));
+            Assert.That(results, Contains.Item("101.0.2.3"));
+        }
+    }
+}
