b7f6e99227707f1f280295d4c79e5c158e3c2b39
foxesknow <seanbatten58@gmail.com>
2026-03-10 17:39:04 +0000

    Question 509

---
+++ csharp/LeetCode/LeetCode/Q509_Fibonacci/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q509_Fibonacci
+{
+    public class Solution
+    {
+        public int Fib(int n) 
+        {
+            return Fib(0, 1, n);
+
+            static int Fib(int first, int second, int n)
+            {
+                if(n==0) return first;
+                if(n==1) return second;
+
+                return Fib(second, first + second, n - 1);
+            }
+        }
+    }
+}
---
+++ csharp/LeetCode/LeetCodeTests/Q509_Fibonacci/SolutionTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeetCode.Q509_Fibonacci;
+using NUnit.Framework;
+
+namespace LeetCodeTests.Q509_Fibonacci
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void Example1()
+        {
+            var s = new Solution();
+            Assert.That(s.Fib(2), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Example2()
+        {
+            var s = new Solution();
+            Assert.That(s.Fib(3), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Example3()
+        {
+            var s = new Solution();
+            Assert.That(s.Fib(4), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Example4()
+        {
+            var s = new Solution();
+            
+            // The largest fib that can fit in a signed 32 bit int is 1,836,311,903
+            Assert.That(s.Fib(46), Is.EqualTo(1_836_311_903));
+        }
+    }
+}
