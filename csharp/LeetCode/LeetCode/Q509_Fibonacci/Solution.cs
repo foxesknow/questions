3c4f8a08e4860e4b136fa5c564e3bd62a55865ec
foxesknow <seanbatten58@gmail.com>
2026-03-12 12:14:41 +0000

    Memoization

--- csharp/LeetCode/LeetCode/Q509_Fibonacci/Solution.cs
+++ csharp/LeetCode/LeetCode/Q509_Fibonacci/Solution.cs
@@ -20,5 +20,25 @@ namespace LeetCode.Q509_Fibonacci
                 return Fib(second, first + second, n - 1);
             }
         }
+
+        public int FibCache(int n) 
+        {
+            Span<int> cache = stackalloc int[50];
+
+            return Fib(cache, n);
+
+            static int Fib(Span<int> cache, int n)
+            {
+                if(n==0) return 0;
+                if(n==1) return 1;
+
+                if(cache[n] != 0) return cache[n];
+
+                var value = Fib(cache, n - 1) + Fib(cache, n - 2);
+                cache[n] = value;
+
+                return value;
+            }
+        }
     }
 }
--- csharp/LeetCode/LeetCodeTests/Q509_Fibonacci/SolutionTests.cs
+++ csharp/LeetCode/LeetCodeTests/Q509_Fibonacci/SolutionTests.cs
@@ -41,5 +41,35 @@ namespace LeetCodeTests.Q509_Fibonacci
             // The largest fib that can fit in a signed 32 bit int is 1,836,311,903
             Assert.That(s.Fib(46), Is.EqualTo(1_836_311_903));
         }
+
+        [Test]
+        public void CacheExample1()
+        {
+            var s = new Solution();
+            Assert.That(s.FibCache(2), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CacheExample2()
+        {
+            var s = new Solution();
+            Assert.That(s.FibCache(3), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CacheExample3()
+        {
+            var s = new Solution();
+            Assert.That(s.FibCache(4), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void CacheExample4()
+        {
+            var s = new Solution();
+            
+            // The largest fib that can fit in a signed 32 bit int is 1,836,311,903
+            Assert.That(s.FibCache(46), Is.EqualTo(1_836_311_903));
+        }
     }
 }
