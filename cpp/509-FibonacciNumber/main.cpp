f138496c4856a759bdcce3171b8553c396d12773
Sean <seanbatten58@gmail.com>
2026-03-18 21:21:15 +0000

    added memoize version

--- cpp/509-FibonacciNumber/main.cpp
+++ cpp/509-FibonacciNumber/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <unordered_map>
+
+using namespace std;
 
 class Solution 
 {
@@ -16,6 +19,28 @@ public:
 
         return first;    
     }
+
+    int fib_cache(int n)
+    {
+        unordered_map<int, int> cache;
+        return fib_cache(cache, n);
+    }
+
+private:
+    int fib_cache(unordered_map<int, int> &cache, int n) 
+    {
+        if(n < 2) return n;
+
+        if(const auto item = cache.find(n); item != cache.end())
+        {
+            return item->second;
+        }
+
+        auto value = fib_cache(cache, n - 1) + fib_cache(cache, n - 2);
+        cache.insert({n, value});
+
+        return value;
+    }
 };
 
 int main()
@@ -25,4 +50,12 @@ int main()
     std::cout << solution.fib(3) << "\n";
     std::cout << solution.fib(4) << "\n";
     std::cout << solution.fib(10) << "\n";
+    std::cout << solution.fib(20) << "\n";
+
+    std::cout << "Memoize\n";
+    std::cout << solution.fib_cache(2) << "\n";
+    std::cout << solution.fib_cache(3) << "\n";
+    std::cout << solution.fib_cache(4) << "\n";
+    std::cout << solution.fib_cache(10) << "\n";
+    std::cout << solution.fib_cache(20) << "\n";
 }
