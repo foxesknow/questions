063317158d1e1940eef8c10808e3cff61289dae1
foxesknow <seanbatten58@gmail.com>
2026-03-27 17:00:32 +0000

    Question 70

---
+++ csharp/LeetCode/LeetCode/Q70_ClimbingStairs/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Q70_ClimbingStairs;
+
+public class Solution
+{
+    public int ClimbStairs(int n) 
+    {
+        Span<int> cache = stackalloc int[n + 1];
+
+        return Run(cache, n);
+
+        static int Run(Span<int> cache, int n)
+        {
+            if(n < 0) return 0;
+            if(n <= 2) return n;
+
+            if(cache[n] is int previous && previous != 0)
+            {
+                return previous;
+            }
+
+            var result = Run(cache, n - 1) + Run(cache, n - 2);
+            cache[n] = result;
+
+            return result;
+        }
+    }
+}
